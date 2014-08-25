ffe9f59f1d800aa78ddad3e6d2f8fe5418247735
Sergey Lyubka <valenok@gmail.com>
2014-08-25 20:44:05 +0100

    Enhancing HTTP parsing and request handling

--- Makefile
+++ Makefile
@@ -5,29 +5,26 @@
 
 NS = ../net_skeleton
 V7 = ../v7
-SLRE = ../slre
 
-SOURCES = engine.c $(NS)/net_skeleton.c $(V7)/v7.c $(SLRE)/slre.c
+SOURCES = engine.c $(NS)/net_skeleton.c $(V7)/v7.c
 BINARY = engine
 
-CFLAGS = -I$(NS) -I$(V7) -I$(SLRE) $(CFLAGS_EXTRA)
+CFLAGS = -I$(NS) -I$(V7) $(CFLAGS_EXTRA) -W -Wall
 
 all: $(BINARY)
 
 $(BINARY): $(SOURCES)
+	cd $(V7) && make v7.c
 	$(CC) $(SOURCES) -o $@ -W -Wall -g $(CFLAGS)
 
 w:
 	wine cl /MD /TC /nologo /DNDEBUG /O1 $(CFLAGS) $(SOURCES)
 
 $(NS)/net_skeleton.c:
-	cd .. && git clone https://github.com/cesanta/net_skeleton
+	-@cd .. && git clone https://github.com/cesanta/net_skeleton
 
-$(V7)/v7.c:
-	cd .. && git clone https://github.com/cesanta/v7
-
-$(SLRE)/slre.c:
-	cd .. && git clone https://github.com/cesanta/slre
+$(V7)/v7.c: $(V7)/v7.h
+	test -d $(V7) || ( cd .. && git clone https://github.com/cesanta/v7 )
 
 clean:
 	rm -rf $(BINARY) *.exe *.obj *.o *.dSYM
--- example.js
+++ example.js
@@ -1,40 +1,66 @@
 // This is an example HTTP/Websocket server
 
-load('http.js');
-
-//print(JSON.stringify(parseHttpRequest('GET / HTTP/1.0\r\n\r\n')));
-// NetEventManager = { run: function() {} }
-
-var options = {
-  listening_port: 8000,
-  // enable_ssl_with_certificate: 'cert.pem',
-  // debug_hexdump_file: '/dev/stdout',
-
-  onstart: function() {
-    print('Starting on port ', this.listening_port);
-  },
-
-  onaccept: function(conn) {
-    print(conn.nc, ' connected');
-  },
-
-  onmessage: function(conn) {
-    var req = parseHttpRequest(conn.data);
-    if (!req) return false;   // Bad request, close the connection
-    if (req.uri) {
-      var numConns = conn.server.connections.keys().length;
-      conn.send('HTTP/1.0 200 OK\r\n\r\n',
-                'URI: ', req.uri, '\nNum connections: ', numConns);
-      return false;   // Close connection
+function parseHttpRequest(str) {
+  var ind = str.indexOf('\r\n\r\n');              // Where HTTP headers end
+  if (ind < 0 && str.length > 8192) return null;  // Request too big
+  if (ind < 14 && ind > 0) return null;           // Request too small
+  if (ind > 0) {
+    var request = str.substr(0, ind + 1);
+    var lines = request.split(/\s*\n\s*/);
+    var firstLine = lines[0].split(' ');
+    var parsed = { method: firstLine[0], uri: firstLine[1], headers: {} };
+    for (var i = 1; i < lines.length; i++) {
+      var pair = lines[i].split(': ', 2);
+      parsed.headers[pair[0]] = pair[1];
     }
-    // Return nothing (keep connection open) and buffer more conn.data
-  },
-  // onpoll: function(conn) { print('poll, conns: ', srv.connections, '\n'),
-
-  onclose: function(conn) {
-    print(conn.nc, ' disconnected');
+    return parsed;
   }
+  return true;
 };
 
-var srv = NetEventManager(options);
+var httpServerConfig = {
+  document_root: '.',    // Current directory
+  listening_port: 8000
+};
+
+var handleWebsocketData = function(conn) {
+};
+
+var handleHttpRequest = function(conn) {
+  var data, request;
+
+  if (conn.is_websocket) return handleWebsocketData(conn);
+
+  request = parseHttpRequest(conn.data);
+  if (!request) return false;   // Bad request, close the connection
+
+  if (request.uri) {
+    var path = httpServerConfig.document_root + request.uri;
+    var f = open(path, 'r');
+    if (f) {
+      conn.send('HTTP/1.0 200 OK\r\nConnection: close\r\n\r\n');
+      while (data = f.read()) conn.send(data);
+      f.close();
+    } else {
+      conn.send('HTTP/1.0 404 Not Found\r\n\r\n');
+    }
+    //var numConns = conn.server.connections.keys().length;
+    //conn.send('HTTP/1.0 200 OK\r\n\r\n',
+    //          'URI: ', req.uri, '\nNum connections: ', req, '\n');
+    return false;   // Close connection
+  }
+  // Return nothing (keep connection open) and buffer more conn.data
+};
+
+// Configure and start
+var srv = NetEventManager({
+  listening_port: httpServerConfig.listening_port,
+  // enable_ssl_with_certificate: 'cert.pem',
+  // debug_hexdump_file: '/dev/stdout',
+  onstart: function() { print('Starting on port ', this.listening_port); },
+  onaccept: function(conn) { print(conn.nc, ' connected'); },
+  onmessage: handleHttpRequest,
+  // onpoll: function(conn) { print('poll, conns: ', srv.connections, '\n'),
+  onclose: function(conn) { print(conn.nc, ' disconnected'); }
+});
 srv.run();
