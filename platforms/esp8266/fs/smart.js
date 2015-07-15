49e62bba268b4627293d96114eec1d406080d940
Marko Mikulicic <mkm@cesanta.com>
2015-07-15 20:44:12 +0100

    Start temp demo also with an empty user conf
        PUBLISHED_FROM=0d890a683afd7e23b9c84e449b3bba56fc6d17a7

--- platforms/esp8266/fs/smart.js
+++ platforms/esp8266/fs/smart.js
@@ -38,7 +38,7 @@ Cloud.store = function(name,val,opts) {
 }
 
 /* demo */
-if (conf.user.demo == 'MCP9808') {
+if (conf.user.demo === undefined || conf.user.demo == 'MCP9808') {
     File.load('I2C.js');
     File.load('MCP9808.js');
     t = new MCP9808(new I2C(14,12), MCP9808.addr(1,1,1));
