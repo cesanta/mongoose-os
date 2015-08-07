2104e545060f238a5cd606bc352b029dc7fc364d
Deomid Ryabkov <rojer@cesanta.com>
2015-08-07 15:46:11 +0300

    Forgotten files from cesanta/dev#838
        PUBLISHED_FROM=a935bf29ae69c24963ce537d256a4a03b635ee7f

---
+++ platforms/esp8266/user/esp_ssl_krypton.h
@@ -0,0 +1,15 @@
+#ifndef ESP_SSL_KRYPTON_INCLUDED
+#define ESP_SSL_KRYPTON_INCLUDED
+
+#include "os_type.h"
+#include "espconn.h"
+
+/* 
+ * We (aim to) provide an API that closely follows escponn_secure_*
+ * but implemented using Krypton.
+ */
+
+sint8 kr_secure_connect(struct espconn *ec);
+sint8 kr_secure_sent(struct espconn *ec, uint8 *psent, uint16 length);
+
+#endif /* ESP_SSL_KRYPTON_INCLUDED */
