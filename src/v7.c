a4017dca18860184140cef08926a550b76c3df5e
Marko Mikulicic <mkm@cesanta.com>
2015-08-12 20:49:38 +0300

    Fix for..in for real
        PUBLISHED_FROM=567208077004f7e9b2918400a4708cada0385580

--- src/v7.c
+++ src/v7.c
@@ -10948,7 +10948,7 @@ static val_t i_eval_stmt(struct v7 *v7, struct ast *a, ast_off_t *pos,
 
       for (; v7_to_object(obj) != NULL;
            obj = v7_object_to_value(
-               v7_is_function(obj) ? v7_to_object(obj)->prototype : NULL)) {
+               v7_is_function(obj) ? NULL : v7_to_object(obj)->prototype)) {
         for (p = v7_to_object(obj)->properties; p; p = p->next, *pos = loop) {
           if (p->attributes & (V7_PROPERTY_HIDDEN | V7_PROPERTY_DONT_ENUM)) {
             continue;
