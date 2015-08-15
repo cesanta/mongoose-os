435351668be1ecb537ccef9361c932fb4c2379d6
Deomid Ryabkov <rojer@cesanta.com>
2015-08-15 18:43:29 +0200

    Move to SDK 1.3.0 + MALLOC_PROVIDED patch
        PUBLISHED_FROM=256a816a79c714cf1fe7b181609157853ba27101

--- platforms/esp8266/make.sh
+++ platforms/esp8266/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SDK=docker.cesanta.com:5000/esp8266-build-oss:1.2.0
+SDK=docker.cesanta.com:5000/esp8266-build-oss:1.3.0-cesanta
 
 V7DIR=$(dirname $(dirname $PWD))
 DIR=/cesanta/${PWD##$V7DIR}
