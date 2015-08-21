678b4cec18c09c85ddbdd462b30e3eabb63fd2bb
Marko Mikulicic <mkm@cesanta.com>
2015-08-21 08:38:04 +0100

    Make timers work on posix port for osx
        PUBLISHED_FROM=9c702de81b9a9cf404493d88f99a3b67ff9f4dba

--- platforms/posix/posix_hal.c
+++ platforms/posix/posix_hal.c
@@ -4,10 +4,17 @@
 #include <signal.h>
 #include <time.h>
 #include <sj_v7_ext.h>
+#ifdef __APPLE__
+#include <sys/time.h>
+#endif
 
 #ifdef _WIN32
 #else
 #include <unistd.h>
+#endif
+
+#ifdef __APPLE__
+v7_val_t *bsd_timer_cb = NULL;
 #endif
 
 size_t sj_get_free_heap_size() {
@@ -42,14 +49,19 @@ void sj_usleep(int usecs) {
 }
 
 void posix_timer_callback(int sig, siginfo_t *si, void *uc) {
+#ifdef __APPLE__
+  sj_call_function(v7, bsd_timer_cb);
+#else
   sj_call_function(v7, si->si_value.sival_ptr);
+#endif
 }
 
 void sj_set_timeout(int msecs, v7_val_t *cb) {
+  struct sigaction sa;
+
 #if defined(SA_SIGINFO) && defined(CLOCK_REALTIME)
   struct sigevent te;
   struct itimerspec its;
-  struct sigaction sa;
   timer_t timer_id;
 
   sa.sa_flags = SA_SIGINFO;
@@ -70,6 +82,25 @@ void sj_set_timeout(int msecs, v7_val_t *cb) {
   its.it_value.tv_nsec = (msecs % 1000) * 1000000;
 
   timer_settime(timer_id, 0, &its, NULL);
+
+#elif defined(__APPLE__)
+  struct itimerval tv;
+
+  bsd_timer_cb = cb;
+
+  tv.it_interval.tv_sec = 0;
+  tv.it_interval.tv_usec = 0;
+  tv.it_value.tv_sec = msecs / 1000;
+  tv.it_value.tv_usec = (msecs % 1000) * 1000;
+
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = posix_timer_callback;
+  sigemptyset(&sa.sa_mask);
+
+  sigaction(SIGALRM, &sa, NULL);
+
+  setitimer(ITIMER_REAL, &tv, NULL);
+
 #else
   /* TODO(lsm): implement this */
   (void) cb;
