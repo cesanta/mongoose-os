82410a00cb7fd3180ed5113a530888f2b555f8c4
Deomid Ryabkov <rojer@cesanta.com>
2015-08-31 14:57:08 +0100

    Move CC3200 SDK build files to //docker
        PUBLISHED_FROM=6498a67609a8c30174509bf3c1e900dddb3a16e5

--- platforms/esp8266/make.sh
+++ platforms/esp8266/make.sh
@@ -3,7 +3,7 @@
 #SDK=docker.cesanta.com:5000/esp8266-build-rtos
 SDK=docker.cesanta.com:5000/esp8266-build-oss:1.3.0-cesanta
 
-V7DIR=$(dirname $(dirname $PWD))
+V7DIR=$(dirname $(dirname $(dirname $PWD)))
 DIR=/cesanta/${PWD##$V7DIR}
 # Note: "//" in "//bin/bash" is a workaround for a boot2docker bug on Windows.
 exec docker run --rm -it -v /$V7DIR:/cesanta ${SDK} //bin/bash -c \
