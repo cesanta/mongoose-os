4451eb0808d30cac7a98ed931432a3c002c29d36
Atsushi Takayama <taka.atsushi@gmail.com>
2015-09-06 19:29:00 +0100

    Add comment on AST_TOO_LARGE error
        PUBLISHED_FROM=b243c1a1d00e6b4ba2fa5943438580ebca8c156f

--- src/v7.c
+++ src/v7.c
@@ -101,6 +101,8 @@ enum v7_err {
  *  - V7_SYNTAX_ERROR if `js_code` in not a valid code. `result` is undefined.
  *  - V7_EXEC_EXCEPTION if `js_code` threw an exception. `result` stores
  *    an exception object.
+ *  - V7_AST_TOO_LARGE if `js_code` contains an AST segment longer than 16 bit.
+ *    `result` is undefined. To avoid this error, build V7 with V7_LARGE_AST.
  */
 enum v7_err v7_exec(struct v7 *, v7_val_t *result, const char *js_code);
 
