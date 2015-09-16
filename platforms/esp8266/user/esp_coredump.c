2bceffe4fb5d3dce8c4ba2e0ac7647e84db3beaa
Marko Mikulicic <mkm@cesanta.com>
2015-09-16 19:22:09 +0300

    Dump exception vectors memory and ROM on RTOS
        PUBLISHED_FROM=2f12da9ababed0ca9f64e9be77e1ccb9c84cc3ab

--- platforms/esp8266/user/esp_coredump.c
+++ platforms/esp8266/user/esp_coredump.c
@@ -36,24 +36,38 @@ static void uart_putdec(int fd, unsigned int n) {
 
 static int core_dump_emit_char_fd = 0;
 static void core_dump_emit_char(char c, void *user_data) {
-  (void) user_data;
+  int *col_counter = (int *) user_data;
 #ifdef RTOS_SDK
   system_soft_wdt_feed();
 #endif
+  (*col_counter)++;
   uart_putchar(core_dump_emit_char_fd, c);
+  if (*col_counter >= 160) {
+    uart_putchar(core_dump_emit_char_fd, '\n');
+    (*col_counter) = 0;
+  }
 }
 
+/* address must be aligned to 4 and size must be multiple of 4 */
 static void emit_core_dump_section(int fd, const char *name, uint32_t addr,
                                    uint32_t size) {
   struct cs_base64_ctx ctx;
+  int col_counter = 0;
   uart_puts(fd, ",\"");
   uart_puts(fd, name);
   uart_puts(fd, "\": {\"addr\": ");
   uart_putdec(fd, addr);
   uart_puts(fd, ", \"data\": \"");
   core_dump_emit_char_fd = fd;
-  cs_base64_init(&ctx, core_dump_emit_char, NULL);
-  cs_base64_update(&ctx, (char *) addr, size);
+  cs_base64_init(&ctx, core_dump_emit_char, &col_counter);
+
+  uint32_t end = addr + size;
+  while (addr < end) {
+    uint32_t buf;
+    buf = *((uint32_t *) addr);
+    addr += sizeof(uint32_t);
+    cs_base64_update(&ctx, (char *) &buf, sizeof(uint32_t));
+  }
   cs_base64_finish(&ctx);
   uart_puts(fd, "\"}");
 }
@@ -67,9 +81,9 @@ void esp_dump_core(int fd, struct regfile *regs) {
   uart_puts(fd, "{\"arch\": \"ESP8266\"");
   emit_core_dump_section(fd, "REGS", (uintptr_t) regs, sizeof(*regs));
   emit_core_dump_section(fd, "DRAM", 0x3FFE8000, 0x18000);
-#ifndef RTOS_SDK
+  /* rtos relocates vectors here */
+  emit_core_dump_section(fd, "VEC", 0x40100000, 0x1000);
   emit_core_dump_section(fd, "ROM", 0x40000000, 0x10000);
-#endif
   uart_puts(fd, "}\n");
 
   /*
