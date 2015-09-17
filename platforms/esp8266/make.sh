9719dad4ccf43e5142c4cbf5449999166d541b4a
Marko Mikulicic <mkm@cesanta.com>
2015-09-17 08:49:44 +0100

    Make ./make.sh make things with spaces
        PUBLISHED_FROM=604705b7649babaa6d7c9e797d24d407c379e957

--- platforms/esp8266/make.sh
+++ platforms/esp8266/make.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+cd $(dirname $0)
 
 if [ "$1" = "RTOS" ]; then
     shift
@@ -9,6 +11,9 @@ fi
 
 V7DIR=$(dirname $(dirname $PWD))
 DIR=/cesanta/${PWD##$V7DIR}
+
 # Note: "//" in "//bin/bash" is a workaround for a boot2docker bug on Windows.
+V=$(for i in "$@" ; do echo $i; done | xargs  -I% echo -n "'%' " )
+
 exec docker run --rm -it -v /$V7DIR:/cesanta ${SDK} //bin/bash -c \
-     "cd $DIR && make $@ && python ./tools/showbreakdown.py"
+     "cd $DIR && make $V && python ./tools/showbreakdown.py"
