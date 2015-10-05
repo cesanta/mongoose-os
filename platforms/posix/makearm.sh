ba91d2af96907724b1cdf103399f0703948e5f84
Marko Mikulicic <mkm@cesanta.com>
2015-10-05 14:42:55 +0300

    Fix makearm.sh script
        PUBLISHED_FROM=a1b60c4169af29c8d72c9437d34a12a12ec402ee

--- platforms/posix/makearm.sh
+++ platforms/posix/makearm.sh
@@ -2,8 +2,14 @@
 
 SDK=docker.cesanta.com:5000/eabihf-arm-build
 
-DEVDIR=$(dirname $(dirname $(dirname $PWD)))
+DEVDIR=$(dirname $(dirname $PWD))
+BASE=""
+
+if [ -L ${DEVDIR}/src/platform.mk ]; then
+    # symlink to common repo
+    DEVDIR=$(dirname ${DEVDIR})
+    BASE=smartjs/
+fi
 
 # Note: "//" in "//bin/bash" is a workaround for a boot2docker bug on Windows.
-exec docker run --rm -it -v ${DEVDIR}:/cesanta ${SDK} //bin/bash -c "cd /cesanta/smartjs/platforms/posix && make $@"
-
+exec docker run --rm -it -v ${DEVDIR}:/cesanta ${SDK} //bin/bash -c "cd /cesanta/${BASE}platforms/posix && make $@"
