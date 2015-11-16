3bf1f462b56c7b3b0ed120fa13054d63b70141fb
Deomid Ryabkov <rojer@cesanta.com>
2015-11-16 19:20:11 +0100

    Commonize LWIP for ESP8266
        PUBLISHED_FROM=85a375b34395038cff2f484d67abae3ba2cf4e3e

--- platforms/esp8266/lwip/common.mk
+++ platforms/esp8266/lwip/common.mk
@@ -55,6 +55,25 @@ define compile
 $(call compile_params,$<,$@)
 endef
 
+# some of these flags works around for gdb 7.5.x stacktrace issue
+# while still allowing -Os to remove padding between data in .rodata
+# section, allowing us to gain about 1k of ram.
+# text section is 4k bigger, but we care more about ram at the moment.
+# TODO(mkm): figure out which flag(s).
+NO_Os_FLAGS= -fno-expensive-optimizations -fno-thread-jumps \
+             -fno-align-functions -fno-align-jumps \
+             -fno-align-loops -fno-align-labels -fno-caller-saves \
+             -fno-crossjumping -fno-cse-follow-jumps -fno-cse-skip-blocks \
+             -fno-delete-null-pointer-checks -fno-devirtualize  \
+             -fno-gcse -fno-gcse-lm -fno-hoist-adjacent-loads \
+             -fno-inline-small-functions -fno-indirect-inlining -fno-partial-inlining \
+             -fno-ipa-cp -fno-ipa-sra -fno-peephole2 -fno-optimize-sibling-calls -fno-optimize-strlen \
+             -fno-reorder-blocks -fno-reorder-blocks-and-partition -fno-reorder-functions \
+             -fno-sched-interblock -fno-sched-spec -fno-rerun-cse-after-loop \
+             -fno-schedule-insns -fno-schedule-insns2 -fno-strict-aliasing -fno-strict-overflow \
+             -fno-tree-builtin-call-dce -fno-tree-switch-conversion -fno-tree-tail-merge \
+             -fno-tree-pre -fno-tree-vrp
+
 CFLAGS := -Wall -Werror -Os $(NO_Os_FLAGS) -g3 -Wpointer-arith -Wl,-EL -fno-inline-functions \
           -nostdlib -mlongcalls -mtext-section-literals  -D__ets__ -DSTATIC=static \
           -Wno-parentheses -DFAST='__attribute__((section(".fast.text")))' \
