b0c52217ed02be7e912189eef5c1635b1fa00c0e
Alexander Alashkin <alexander.alashkin@cesanta.com>
2015-12-18 08:44:24 +0000

    Return clubby and js prompt to posix port
        PUBLISHED_FROM=bf6d3794b3da9a23a415f72b41192ee65aa6cb1e

--- smartjs/platforms/posix/main.c
+++ smartjs/platforms/posix/main.c
@@ -60,9 +60,9 @@ static void pre_init(struct v7 *v7) {
 }
 
 static void post_init(struct v7 *v7) {
-  sj_prompt_init(v7);
   init_device(v7);
   run_init_script(v7);
+  sj_prompt_init(v7);
   do {
     /*
      * Now waiting until mongoose has active connections
