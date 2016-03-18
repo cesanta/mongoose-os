3429f35eeeaa6bd51d8dbba8c3fd78e54b1c7c69
Deomid Ryabkov <rojer@cesanta.com>
2016-03-18 13:35:20 +0200

    Handle 32-bit microsecond counter overflow for ESP
    PUBLISHED_FROM=49bf58e339c17de3e1de2a154f25565e5f5f51f6

--- smartjs/platforms/esp8266/user/esp_libc.c
+++ smartjs/platforms/esp8266/user/esp_libc.c
@@ -501,13 +501,23 @@ void _exit(int status) {
   abort();
 }
 
+/*
+ * This will prevent counter wrap if time is read regularly.
+ * At least Mongoose poll queries time, so we're covered.
+ */
 int _gettimeofday_r(struct _reent *r, struct timeval *tp, void *tzp) {
+  static uint32_t prev_time = 0;
+  static uint32_t num_overflows = 0;
   uint32_t time = system_get_time();
+  uint64_t time64 = time;
+  if (prev_time > 0 && time < prev_time) num_overflows++;
+  time64 += (((uint64_t) num_overflows) * (1ULL << 32));
+  tp->tv_sec = time64 / 1000000ULL;
+  tp->tv_usec = time64 % 1000000ULL;
+  prev_time = time;
+  return 0;
   (void) r;
   (void) tzp;
-  tp->tv_sec = time / 1000000;
-  tp->tv_usec = time % 1000000;
-  return 0;
 }
 
 unsigned long os_random(void);
