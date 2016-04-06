ebe10a13a9c4ae2675c85b663389b69ec22e6051
Maxim Ignatenko <imax@cesanta.com>
2016-04-06 11:42:11 +0200

    Expose docker socket to fwbuild backend
    PUBLISHED_FROM=3b0f9a3f58641c564e44d34f9d884e196f9d8f46

---
+++ smartjs/src/sj_utils.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#include "sj_uart.h"
+#include "device_config.h"
+
+#ifdef MG_ENABLE_SSL
+enum v7_err fill_ssl_connect_opts(struct v7 *v7, v7_val_t opts, int force_ssl,
+                                  struct mg_connect_opts *copts) {
+  enum v7_err rcode = V7_OK;
+
+  v7_val_t v_use_ssl = v7_get(v7, opts, "use_ssl", ~0);
+  v7_val_t v_ca_cert = v7_get(v7, opts, "ssl_ca_cert", ~0);
+  v7_val_t v_cert = v7_get(v7, opts, "ssl_cert", ~0);
+  v7_val_t v_server_name = v7_get(v7, opts, "ssl_server_name", ~0);
+
+  if (!v7_is_undefined(v_ca_cert) && !v7_is_string(v_ca_cert)) {
+    rcode = v7_throwf(v7, "TypeError", "ssl_ca_cert must be a string");
+    goto clean;
+  }
+  if (!v7_is_undefined(v_cert) && !v7_is_string(v_cert)) {
+    rcode = v7_throwf(v7, "TypeError", "ssl_cert must be a string");
+    goto clean;
+  }
+  if (!v7_is_undefined(v_server_name) && !v7_is_string(v_server_name)) {
+    rcode = v7_throwf(v7, "TypeError", "ssl_cerver_name must be a string");
+    goto clean;
+  }
+
+  copts->ssl_ca_cert = v7_to_cstring(v7, &v_ca_cert);
+  copts->ssl_cert = v7_to_cstring(v7, &v_cert);
+  copts->ssl_server_name = v7_to_cstring(v7, &v_server_name);
+
+  if ((force_ssl ||
+       (v7_is_boolean(v_use_ssl) && v7_to_boolean(v_use_ssl) != 0)) &&
+      copts->ssl_ca_cert == NULL) {
+    /* Defaults to configuration */
+    copts->ssl_ca_cert = get_cfg()->tls.ca_file;
+  }
+
+clean:
+  return rcode;
+}
+#endif
---
+++ smartjs/src/sj_utils.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef SJ_UTILS_H
+#define SJ_UTILS_H
+
+#include "v7/v7.h"
+#include "mongoose/mongoose.h"
+
+enum v7_err fill_ssl_connect_opts(struct v7 *v7, v7_val_t opts, int force_ssl,
+                                  struct mg_connect_opts *copts);
+
+#endif /* SJ_UTILS_H */
