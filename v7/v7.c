86411788b4a883c0476e54b85f673d2e471e4bf9
Marko Mikulicic <mkm@cesanta.com>
2016-04-20 04:48:00 -0400

    Implement V7 user data and destructors
    PUBLISHED_FROM=bc59d8f17920c924e5d9d76f25b683b76cab3ad4

--- v7/v7.c
+++ v7/v7.c
@@ -2351,11 +2351,13 @@ typedef unsigned short v7_prop_attr_t;
 #define _V7_PROPERTY_HIDDEN (1 << 5)
 /* property not managed by V7 HEAP */
 #define _V7_PROPERTY_OFF_HEAP (1 << 6)
+/* special property holding user data and destructor cb */
+#define _V7_PROPERTY_USER_DATA_AND_DESTRUCTOR (1 << 7)
 /*
  * not a property attribute, but a flag for `v7_def()`. It's here in order to
  * keep all offsets in one place
  */
-#define _V7_DESC_PRESERVE_VALUE (1 << 7)
+#define _V7_DESC_PRESERVE_VALUE (1 << 8)
 
 /*
  * Internal helpers for `V7_DESC_...` macros
@@ -2387,6 +2389,9 @@ typedef unsigned long v7_prop_attr_desc_t;
 
 #define _V7_DESC_HIDDEN(v) _V7_MK_DESC(v, _V7_PROPERTY_HIDDEN)
 #define _V7_DESC_OFF_HEAP(v) _V7_MK_DESC(v, _V7_PROPERTY_OFF_HEAP)
+
+/* See `v7_set_destructor_cb` */
+typedef void(v7_destructor_cb_t)(void *ud);
 
 /* Make an empty object */
 v7_val_t v7_mk_object(struct v7 *v7);
@@ -2486,6 +2491,44 @@ int v7_is_instanceof(struct v7 *v7, v7_val_t o, const char *c);
 
 /* Returns true if the object is an instance of a given constructor. */
 int v7_is_instanceof_v(struct v7 *v7, v7_val_t o, v7_val_t c);
+
+/*
+ * Associates an opaque C value (anything that can be casted to a `void * )
+ * with an object.
+ *
+ * You can achieve a similar effect by just setting a special property with
+ * a foreign value (see `v7_mk_foreign`), except user data offers the following
+ * advantages:
+ *
+ * 1. You don't have to come up with some arbitrary "special" property name.
+ * 2. JS scripts cannot access user data by mistake via property lookup.
+ * 3. The user data is available to the destructor. When the desctructor is
+ *    invoked you cannot access any of its properties.
+ * 4. Allows the implementation to use a more compact encoding
+ *
+ * Does nothing if `obj` is not a mutable object.
+ */
+void v7_set_user_data(struct v7 *v7, v7_val_t obj, void *ud);
+
+/*
+ * Get the opaque user data set with `v7_set_user_data`.
+ *
+ * Returns NULL if there is no user data set or if `obj` is not an object.
+ */
+void *v7_get_user_data(struct v7 *v7, v7_val_t obj);
+
+/*
+ * Register a callback which will be invoked when a given object gets
+ * reclaimed by the garbage collector.
+ *
+ * The callback will be invoked while garbage collection is still in progress
+ * and hence the internal state of the JS heap is in an undefined state.
+ * The callback thus cannot perform any calls to the V7 API and will receive
+ * only the user data associated with the destructed object.
+ *
+ * The intended use case is to reclaim resources allocated by C code.
+ */
+void v7_set_destructor_cb(struct v7 *v7, v7_val_t obj, v7_destructor_cb_t *d);
 
 #if defined(__cplusplus)
 }
@@ -3250,6 +3293,7 @@ typedef unsigned char v7_obj_attr_t;
 #define V7_OBJ_DENSE_ARRAY (1 << 1)    /* TODO(mkm): store in some tag */
 #define V7_OBJ_FUNCTION (1 << 2)       /* function object */
 #define V7_OBJ_OFF_HEAP (1 << 3)       /* object not managed by V7 HEAP */
+#define V7_OBJ_HAS_DESTRUCTOR (1 << 4) /* has user data */
 
 /*
  * JavaScript value is either a primitive, or an object.
@@ -4173,6 +4217,9 @@ V7_PRIVATE struct v7_object *obj_prototype(struct v7 *v7,
 V7_PRIVATE val_t obj_prototype_v(struct v7 *v7, val_t obj);
 
 V7_PRIVATE int is_prototype_of(struct v7 *v7, val_t o, val_t p);
+
+/* Get the property holding user data and destructor, or NULL */
+V7_PRIVATE struct v7_property *get_user_data_property(v7_val_t obj);
 
 #endif /* CS_V7_SRC_OBJECT_H_ */
 #ifdef V7_MODULE_LINES
@@ -15209,6 +15256,7 @@ static void generic_object_destructor(struct v7 *v7, void *ptr) {
   struct v7_property *p;
   struct mbuf *abuf;
 
+  /* TODO(mkm): make regexp use user data API */
   p = v7_get_own_property2(v7, v7_object_to_value(&o->base), "", 0,
                            _V7_PROPERTY_HIDDEN);
 
@@ -15226,6 +15274,20 @@ static void generic_object_destructor(struct v7 *v7, void *ptr) {
         ((abuf = (struct mbuf *) v7_to_foreign(p->value)) != NULL)) {
       mbuf_free(abuf);
       free(abuf);
+    }
+  }
+
+  if (o->base.attributes & V7_OBJ_HAS_DESTRUCTOR) {
+    struct v7_property *p;
+    for (p = o->base.properties; p != NULL; p = p->next) {
+      if (p->attributes & _V7_PROPERTY_USER_DATA_AND_DESTRUCTOR) {
+        if (v7_is_foreign(p->name)) {
+          v7_destructor_cb_t *cb =
+              (v7_destructor_cb_t *) v7_to_foreign(p->name);
+          cb(v7_to_foreign(p->value));
+        }
+        break;
+      }
     }
   }
 
@@ -17711,6 +17773,80 @@ v7_val_t v7_set_proto(struct v7 *v7, v7_val_t obj, v7_val_t proto) {
   }
 }
 
+V7_PRIVATE struct v7_property *get_user_data_property(v7_val_t obj) {
+  struct v7_property *p;
+  struct v7_object *o;
+  if (!v7_is_object(obj)) return NULL;
+  o = v7_to_object(obj);
+
+  for (p = o->properties; p != NULL; p = p->next) {
+    if (p->attributes & _V7_PROPERTY_USER_DATA_AND_DESTRUCTOR) {
+      return p;
+    }
+  }
+
+  return NULL;
+}
+
+/*
+ * Returns the user data property structure associated with obj, or NULL if
+ * `obj` is not an object.
+ */
+static struct v7_property *get_or_create_user_data_property(struct v7 *v7,
+                                                            v7_val_t obj) {
+  struct v7_property *p = get_user_data_property(obj);
+  struct v7_object *o;
+
+  if (p != NULL) return p;
+
+  if (!v7_is_object(obj)) return NULL;
+  o = v7_to_object(obj);
+  v7_own(v7, &obj);
+  p = v7_mk_property(v7);
+  v7_disown(v7, &obj);
+
+  p->attributes |= _V7_PROPERTY_USER_DATA_AND_DESTRUCTOR | _V7_PROPERTY_HIDDEN;
+
+  p->next = o->properties;
+  o->properties = p;
+
+  return p;
+}
+
+void v7_set_user_data(struct v7 *v7, v7_val_t obj, void *ud) {
+  struct v7_property *p = get_or_create_user_data_property(v7, obj);
+  if (p == NULL) return;
+  p->value = v7_mk_foreign(ud);
+}
+
+void *v7_get_user_data(struct v7 *v7, v7_val_t obj) {
+  struct v7_property *p = get_user_data_property(obj);
+  (void) v7;
+  if (p == NULL) return NULL;
+  return v7_to_foreign(p->value);
+}
+
+void v7_set_destructor_cb(struct v7 *v7, v7_val_t obj, v7_destructor_cb_t *d) {
+  struct v7_property *p = get_or_create_user_data_property(v7, obj);
+  struct v7_object *o;
+  union {
+    void *v;
+    v7_destructor_cb_t *f;
+  } fu;
+
+  if (p == NULL) return;
+
+  o = v7_to_object(obj);
+  if (d != NULL) {
+    o->attributes |= V7_OBJ_HAS_DESTRUCTOR;
+    fu.f = d;
+    p->name = v7_mk_foreign(fu.v);
+  } else {
+    o->attributes &= ~V7_OBJ_HAS_DESTRUCTOR;
+    p->name = v7_mk_undefined();
+  }
+}
+
 /* }}} Object prototypes */
 #ifdef V7_MODULE_LINES
 #line 1 "./src/regexp.c"
@@ -17763,6 +17899,7 @@ V7_PRIVATE struct v7_regexp *v7_to_regexp(struct v7 *v7, val_t v) {
   int is = v7_is_regexp(v7, v);
   (void) is;
   assert(is == 1);
+  /* TODO(mkm): make regexp use user data API */
   p = v7_get_own_property2(v7, v, "", 0, _V7_PROPERTY_HIDDEN);
   assert(p != NULL);
   return (struct v7_regexp *) v7_to_pointer(p->value);
@@ -17771,6 +17908,7 @@ V7_PRIVATE struct v7_regexp *v7_to_regexp(struct v7 *v7, val_t v) {
 int v7_is_regexp(struct v7 *v7, val_t v) {
   struct v7_property *p;
   if (!v7_is_generic_object(v)) return 0;
+  /* TODO(mkm): make regexp use user data API */
   p = v7_get_own_property2(v7, v, "", 0, _V7_PROPERTY_HIDDEN);
   if (p == NULL) return 0;
   return (p->value & V7_TAG_MASK) == V7_TAG_REGEXP;
--- v7/v7.h
+++ v7/v7.h
@@ -819,11 +819,13 @@ typedef unsigned short v7_prop_attr_t;
 #define _V7_PROPERTY_HIDDEN (1 << 5)
 /* property not managed by V7 HEAP */
 #define _V7_PROPERTY_OFF_HEAP (1 << 6)
+/* special property holding user data and destructor cb */
+#define _V7_PROPERTY_USER_DATA_AND_DESTRUCTOR (1 << 7)
 /*
  * not a property attribute, but a flag for `v7_def()`. It's here in order to
  * keep all offsets in one place
  */
-#define _V7_DESC_PRESERVE_VALUE (1 << 7)
+#define _V7_DESC_PRESERVE_VALUE (1 << 8)
 
 /*
  * Internal helpers for `V7_DESC_...` macros
@@ -855,6 +857,9 @@ typedef unsigned long v7_prop_attr_desc_t;
 
 #define _V7_DESC_HIDDEN(v) _V7_MK_DESC(v, _V7_PROPERTY_HIDDEN)
 #define _V7_DESC_OFF_HEAP(v) _V7_MK_DESC(v, _V7_PROPERTY_OFF_HEAP)
+
+/* See `v7_set_destructor_cb` */
+typedef void(v7_destructor_cb_t)(void *ud);
 
 /* Make an empty object */
 v7_val_t v7_mk_object(struct v7 *v7);
@@ -954,6 +959,44 @@ int v7_is_instanceof(struct v7 *v7, v7_val_t o, const char *c);
 
 /* Returns true if the object is an instance of a given constructor. */
 int v7_is_instanceof_v(struct v7 *v7, v7_val_t o, v7_val_t c);
+
+/*
+ * Associates an opaque C value (anything that can be casted to a `void * )
+ * with an object.
+ *
+ * You can achieve a similar effect by just setting a special property with
+ * a foreign value (see `v7_mk_foreign`), except user data offers the following
+ * advantages:
+ *
+ * 1. You don't have to come up with some arbitrary "special" property name.
+ * 2. JS scripts cannot access user data by mistake via property lookup.
+ * 3. The user data is available to the destructor. When the desctructor is
+ *    invoked you cannot access any of its properties.
+ * 4. Allows the implementation to use a more compact encoding
+ *
+ * Does nothing if `obj` is not a mutable object.
+ */
+void v7_set_user_data(struct v7 *v7, v7_val_t obj, void *ud);
+
+/*
+ * Get the opaque user data set with `v7_set_user_data`.
+ *
+ * Returns NULL if there is no user data set or if `obj` is not an object.
+ */
+void *v7_get_user_data(struct v7 *v7, v7_val_t obj);
+
+/*
+ * Register a callback which will be invoked when a given object gets
+ * reclaimed by the garbage collector.
+ *
+ * The callback will be invoked while garbage collection is still in progress
+ * and hence the internal state of the JS heap is in an undefined state.
+ * The callback thus cannot perform any calls to the V7 API and will receive
+ * only the user data associated with the destructed object.
+ *
+ * The intended use case is to reclaim resources allocated by C code.
+ */
+void v7_set_destructor_cb(struct v7 *v7, v7_val_t obj, v7_destructor_cb_t *d);
 
 #if defined(__cplusplus)
 }
