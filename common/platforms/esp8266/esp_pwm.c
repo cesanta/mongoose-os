030040316b0a01456243e973db704895c8bed825
Alexander Alashkin <alexander.alashkin@cesanta.com>
2016-04-25 13:44:24 +0100

    Fix PWM doc
    PUBLISHED_FROM=ea24230d81c11eec000c60166b86b73a576f3f5f

--- common/platforms/esp8266/esp_pwm.c
+++ common/platforms/esp8266/esp_pwm.c
@@ -22,14 +22,14 @@
  *
  * PWM.set(pin, period, duty)
  *   pin:    GPIO number.
- *   period: Period, in microseconds, 20 is the minimum supported and any number
- *             will be rounded down to the nearest multiple of 10. period = 0
- *             disables PWM on the pin (duty = 0 is similar but does not perform
- *             internal cleanup).
+ *   period: Period, in microseconds, 100 is the minimum supported and any
+ *             number will be rounded down to the nearest multiple of 50.
+ *             period = 0 disables PWM on the pin (duty = 0 is similar but
+ *             does not perform internal cleanup).
  *   duty:   How many microseconds to spend in "1" state. Must be between 0 and
  *             period (inclusive). 0 is "always off", period is "always on",
  *             period / 2 is a square wave. Number will be rounded down to the
- *             nearest multiple of 10.
+ *             nearest multiple of 50.
  *
  *
  * Is it better than the PWM implementation that comes with the SDK? Yes it is.
