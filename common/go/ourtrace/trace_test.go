0b82fb4d2274e89ede8bddcc26bcb1b3255f4f65
Marko Mikulicic <mkm@cesanta.com>
2016-05-09 14:14:08 +0100

    Integrate with appdash
    PUBLISHED_FROM=a6d2a58d20493d63b022c4c7291d1f14fc190550

--- common/go/ourtrace/trace_test.go
+++ common/go/ourtrace/trace_test.go
@@ -11,7 +11,8 @@ func TestNewContext(t *testing.T) {
 	traceID := uint64(1)
 	spanID := uint64(2)
 
-	tr := New("foo", "bar")
+	var tr trace.Trace
+	tr = New("foo", "bar")
 	tr.SetTraceInfo(traceID, spanID)
 
 	ctx := trace.NewContext(context.Background(), tr)
@@ -27,4 +28,11 @@ func TestNewContext(t *testing.T) {
 	if got, want := gotr.SpanID, spanID; got != want {
 		t.Errorf("got: %d, want: %d", got, want)
 	}
+}
+
+func TestNullSampler(t *testing.T) {
+	var s TraceSampler
+	s = nullTraceSampler{}
+	r := s.Sample("foo", "bar")
+	r.Finish()
 }
