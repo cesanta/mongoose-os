446aa1877b2e3c82b5159679df89697da98db0ec
Deomid Ryabkov <rojer@cesanta.com>
2016-06-02 15:37:26 +0100

    Add config and file reading example to c_hello
    PUBLISHED_FROM=b7b3be780d616db883fc42a3b995c583b2e400fa

--- fw/examples/c_hello/README.md
+++ fw/examples/c_hello/README.md
@@ -3,12 +3,16 @@ This is a C-only Mongoose IoT application.
 Compile with `PLATFORM` set:
 
 ```
-make PLATFORM=esp8266
+make PLATFORM=esp8266 && \
+  FNC --platform=esp8266 --port=/dev/ttyUSB0 --flash-baud-rate=1500000 --flash firmware/*-last.zip --console
 ```
 
 or
 
 
 ```
-make PLATFORM=cc3200
+make PLATFORM=cc3200 && \
+  FNC --platform=cc3200 --port=/dev/ttyUSB1 --flash firmware/*-last.zip --console
 ```
+
+FNC is the [Flash 'n Chips](https://github.com/cesanta/fnc) flashing tool, it is optional (but convenient, try it).
--- fw/examples/c_hello/src/hello.c
+++ fw/examples/c_hello/src/hello.c
@@ -3,20 +3,40 @@
 #include "common/platform.h"
 #include "fw/src/sj_app.h"
 #include "fw/src/sj_gpio.h"
+#include "fw/src/device_config.h"
+#include "sys_config.h"
 
 #if CS_PLATFORM == CS_P_ESP_LWIP
 #define GPIO 12
 #elif CS_PLATFORM == CS_P_CC3200
-#define GPIO 64
+#define GPIO 64  /* The red LED on LAUNCHXL */
 #else
 #error Unknown platform
 #endif
 
 int sj_app_init(struct v7 *v7) {
-  (void) v7;
-  printf("Hello, world!\n");
-  sj_gpio_set_mode(GPIO, GPIO_MODE_OUTPUT, GPIO_PULL_FLOAT);
-  sj_gpio_write(GPIO, GPIO_LEVEL_HIGH);
 
+  { /* Print a message using a value from config. */
+    printf("Hello, %s!\n", get_cfg()->hello.who);
+  }
+
+  { /* Turn on LED. */
+    sj_gpio_set_mode(GPIO, GPIO_MODE_OUTPUT, GPIO_PULL_FLOAT);
+    sj_gpio_write(GPIO, GPIO_LEVEL_HIGH);
+  }
+
+  { /* Read a file. */
+    FILE *fp = fopen("README.txt", "r");
+    if (fp != NULL) {
+      char buf[100];
+      int n = fread(buf, 1, sizeof(buf), fp);
+      if (n > 0) {
+        fwrite(buf, 1, n, stdout);
+      }
+      fclose(fp);
+    }
+  }
+
+  (void) v7;
   return 1;
 }
