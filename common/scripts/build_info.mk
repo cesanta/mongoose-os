2423ceb1ae57f01416ab6f12460eaa147bcaf55d
rojer <rojer@rojer.me>
2016-06-16 15:34:44 +0100

    Use $(PYTHON) to run fw_meta.py in build_info.mk
    PUBLISHED_FROM=02d4970e9b96046d6e44e52dfa3ed4c6cf0bb785

--- common/scripts/build_info.mk
+++ common/scripts/build_info.mk
@@ -1,3 +1,4 @@
+PYTHON ?= python
 BUILD_DIR ?= ./build
 BUILD_INFO_C ?= $(BUILD_DIR)/build_info.c
 BUILD_INFO_JSON ?= $(BUILD_DIR)/build_info.json
@@ -5,6 +6,6 @@ FW_META_CMD ?= $(REPO_PATH)/common/tools/fw_meta.py
 
 $(BUILD_INFO_C) $(BUILD_INFO_JSON):
 	$(vecho) "GEN   $@"
-	$(Q) $(FW_META_CMD) gen_build_info \
+	$(Q) $(PYTHON) $(FW_META_CMD) gen_build_info \
 	  --c_output=$(BUILD_INFO_C) \
 	  --json_output=$(BUILD_INFO_JSON)
