f836cac1beffcd85ea76d0cd7f765a9faea3ad78
Alexander Alashkin <alexander.alashkin@cesanta.com>
2016-06-17 11:30:10 +0300

    Adapt HWTests for T02 and add SPI hwtest
    PUBLISHED_FROM=e043d4048200e06506afce04864a2249796f8a98

--- fw/src/js/MPL115A1.js
+++ fw/src/js/MPL115A1.js
@@ -1,9 +1,10 @@
 function MPL115A1() {
+  this.vars = {};
   var bus = new SPI();
-  var a0 = convert(bus.tran(0x88,1) << 8 | bus.tran(0x8A,1), 1 << 3, 15, 0xFFFF);
-  var b1 = convert(bus.tran(0x8C,1) << 8 | bus.tran(0x8E,1), 1 << 13, 15, 0xFFFF);
-  var b2 = convert(bus.tran(0x90,1) << 8 | bus.tran(0x92,1), 1 << 14, 15, 0xFFFF);
-  var c12 = convert((bus.tran(0x94,1) << 8 | bus.tran(0x96,1)) >> 2, 1 << 22, 13, 0x3FFF);
+  this.vars.a0 = convert(bus.tran(0x88,1) << 8 | bus.tran(0x8A,1), 1 << 3, 15, 0xFFFF);
+  this.vars.b1 = convert(bus.tran(0x8C,1) << 8 | bus.tran(0x8E,1), 1 << 13, 15, 0xFFFF);
+  this.vars.b2 = convert(bus.tran(0x90,1) << 8 | bus.tran(0x92,1), 1 << 14, 15, 0xFFFF);
+  this.vars.c12 = convert((bus.tran(0x94,1) << 8 | bus.tran(0x96,1)) >> 2, 1 << 22, 13, 0x3FFF);
 
   function convert(n, fcoef, neg_pos, mask) {
     var res = 1;
@@ -17,14 +18,19 @@ function MPL115A1() {
     return res;
   }
 
+  this.vars.p_adc = 0;
+  this.vars.t_adc = 0;
+
   this.getPressure = function () {
-    bus.tran(0x24);
+    bus.tran(0x24, 1);
     usleep(3000);
 
-    var p_adc = (bus.tran(0x80, 1) << 8 | bus.tran(0x82,1)) >> 6;
-    var t_adc = (bus.tran(0x84, 1) << 8 | bus.tran(0x86,1)) >> 6;
+    this.vars.p_adc = (bus.tran(0x80, 1) << 8 | bus.tran(0x82,1)) >> 6;
+    this.vars.t_adc = (bus.tran(0x84, 1) << 8 | bus.tran(0x86,1)) >> 6;
 
-    var p_comp = a0 + (b1 + c12*t_adc)*p_adc + b2 * t_adc;
+    var p_comp = this.vars.a0 +
+                 (this.vars.b1 + this.vars.c12 * this.vars.t_adc) * this.vars.p_adc +
+                 this.vars.b2 * this.vars.t_adc;
     var pressure = p_comp * (115 - 50) / 1023 + 50;
 
     return pressure;
