de15aaf6135cbb90d8129cdb92d2cf05179fa045
Deomid Ryabkov <rojer@cesanta.com>
2016-07-28 20:10:25 +0200

    ESP makefile refactoring: fix linking order
    Linking order is important for correct weak symbol resolution.
    Now, we put MGIOT stuff into a library and user files, if any, appear
    first on the list.
    
    Also cleaned up creation of FS: make sure stuff from APP_FS_PATH
    overrides system stuff.
    
    Removed symcheck, as it complicates things for unclear benefit.
    
    PUBLISHED_FROM=4e07acdf33b67a972f1c81d2b29674d287815637

--- common/platforms/esp8266/common.mk
+++ common/platforms/esp8266/common.mk
@@ -19,7 +19,7 @@ CC_WRAPPER ?=
 define link
 $(vecho) "LD    $@"
 $(Q) $(CC_WRAPPER) $(LD) $(LIBDIRS) -T$(LD_SCRIPT) $(LDFLAGS) -o $@ \
--Wl,--start-group $< $(LIBS) $1 -Wl,--end-group
+-Wl,--start-group $1 -Wl,--end-group
 endef
 
 define compile_params
