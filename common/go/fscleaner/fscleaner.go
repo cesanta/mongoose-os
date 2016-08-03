b9d9da946e3bd2984cdbf7009145cd8e4883d7fe
Marko Mikulicic <mkm@cesanta.com>
2016-08-03 15:26:29 +0100

    Delete old files
    PUBLISHED_FROM=7bc0392b90176166c02a7a57aaf4bccd1110106f

---
+++ common/go/fscleaner/fscleaner.go
@@ -0,0 +1,65 @@
+package fscleaner
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/cesanta/errors"
+	"github.com/golang/glog"
+	"golang.org/x/net/context"
+)
+
+func clean(glob string, minAge time.Duration) error {
+	now := time.Now()
+	glog.Infof("cleaning %q", glob)
+
+	top, err := filepath.Glob(glob)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	for _, d := range top {
+		s, err := os.Stat(d)
+		// ignore files deleted by other agents running on the same shared volume
+		if os.IsNotExist(err) {
+			continue
+		}
+		if err != nil {
+			return errors.Trace(err)
+		}
+		age := now.Sub(s.ModTime())
+		glog.V(2).Infof("age of %q: %s", d, age)
+		if age > minAge {
+			glog.Infof("deleting %q", d)
+			os.RemoveAll(d)
+		}
+	}
+
+	return nil
+}
+
+// Run starts a filesystem cleaning goroutine
+func Run(ctx context.Context, glob string, period, minAge time.Duration) {
+	go func() {
+		ticker := time.NewTicker(period)
+		defer ticker.Stop()
+
+		err := clean(glob, minAge)
+		if err != nil {
+			glog.Warning(err)
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				err := clean(glob, minAge)
+				if err != nil {
+					glog.Warning(err)
+				}
+			}
+		}
+	}()
+}
