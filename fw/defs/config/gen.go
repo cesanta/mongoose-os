bb2c672f94d468a2b1d06de90246dafd158d3fe4
Dmitry Frank <dmitry.frank@cesanta.com>
2016-09-13 12:43:42 +0100

    Move clubbygen functionality to miot
    It addresses 2/3 points of https://github.com/cesanta/dev/issues/5010
    
    The next step is to make it able to parse yaml directly.
    
    PUBLISHED_FROM=6ed16432a453d7d3c28f812ec0d6aff1f2f8bf62

--- fw/defs/config/gen.go
+++ fw/defs/config/gen.go
@@ -1,5 +1,5 @@
 package config
 
 //go:generate make -C .. config.service.json
-//go:generate clubbygen --input ../config.service.json --lang go
-//go:generate clubbygen --input ../config.service.json --lang go --strict
+//go:generate miot clubbygen --input ../config.service.json --lang go
+//go:generate miot clubbygen --input ../config.service.json --lang go --strict
--- fw/defs/vars/gen.go
+++ fw/defs/vars/gen.go
@@ -1,5 +1,5 @@
 package vars
 
 //go:generate make -C .. vars.service.json
-//go:generate clubbygen --input ../vars.service.json --lang go
-//go:generate clubbygen --input ../vars.service.json --lang go --strict
+//go:generate miot clubbygen --input ../vars.service.json --lang go
+//go:generate miot clubbygen --input ../vars.service.json --lang go --strict
