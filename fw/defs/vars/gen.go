c1318c40bf52345cd9c35bd2eb9b2dc29d510bb3
Dmitry Frank <dmitry.frank@cesanta.com>
2016-09-13 20:55:21 +0100

    Add yaml support to miot clubbygen
    Closes https://github.com/cesanta/dev/issues/5010
    
    PUBLISHED_FROM=4f520c3b769ad9881d276a35c04e02aff5c0608b

--- fw/defs/vars/gen.go
+++ fw/defs/vars/gen.go
@@ -1,5 +1,4 @@
 package vars
 
-//go:generate make -C .. vars.service.json
-//go:generate miot clubbygen --input ../vars.service.json --lang go
-//go:generate miot clubbygen --input ../vars.service.json --lang go --strict
+//go:generate miot clubbygen --input ../vars.service.yaml --lang go
+//go:generate miot clubbygen --input ../vars.service.yaml --lang go --strict
