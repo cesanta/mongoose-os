14b9ab90e7b47a92670ee337b4b9311c9c6a3fef
Deomid Ryabkov <rojer@cesanta.com>
2016-10-14 07:50:00 +0100

    MG_DISABLE_DAV -> MG_ENABLE_HTTP_WEBDAV
    PUBLISHED_FROM=62267ea0a8e10d8ba7bad590d1a56b179bcffce9

--- fw/common.mk
+++ fw/common.mk
@@ -17,14 +17,12 @@ COMMON_V7_FEATURES = \
 
 MG_FEATURES_TINY = \
                    -DMG_DISABLE_JSON_RPC \
-                   -DMG_DISABLE_COAP \
                    -DMG_DISABLE_SYNC_RESOLVER \
                    -DMG_DISABLE_HTTP_DIGEST_AUTH \
                    -DMG_DISABLE_MD5 \
                    -DMG_DISABLE_SOCKETPAIR \
                    -DMG_DISABLE_HTTP_KEEP_ALIVE \
                    -DMG_DISABLE_SSI \
-                   -DMG_DISABLE_DAV \
                    -DMG_ENABLE_HTTP_STREAMING_MULTIPART \
                    -DMG_ENABLE_MQTT \
                    -DMG_NO_BSD_SOCKETS
