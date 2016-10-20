8b55d2479d09316d1084911eb7f0b48c216e2b05
wallaroo <michele.giacomelli@gmail.com>
2016-10-20 07:46:00 +0200

    fix image size

--- fw/src/js/sys_init.js
+++ fw/src/js/sys_init.js
@@ -3,16 +3,8 @@
 print('\nMongoose IoT Firmware - see documentation at',
       'https://docs.cesanta.com/mongoose-iot/\n');
 
-if (Sys.conf.clubby.device_id) {
-    print('Device credentials: ', {
-      device_psk: Sys.conf.clubby.device_psk,
-      device_id: Sys.conf.clubby.device_id,
-    }, '\n');
-}
-
 if (File.exists('app.js')) {
   File.eval('app.js');
 } else if (File.exists('demo.js')) {
   print('No app.js, running demo...');
-  File.eval('demo.js');
 }
