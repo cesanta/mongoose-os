dcdebd3e5e5042edd1869331b736948d439a6ac6
Dmitry Frank <dmitry.frank@cesanta.com>
2016-11-07 19:17:33 +0000

    Get rid of REPO_ROOT in docker_common.mk
    PUBLISHED_FROM=4b44d7b8ea7674f9de8b8ea841f9aaa65365a7f1

--- fw/docker_common.mk
+++ fw/docker_common.mk
@@ -1,27 +1,21 @@
 # Common rules for "outer" Makefiles, i.e. the ones that invoke Docker.
 #
 # App must define its name (APP) and path to MIOT repo (MIOT_PATH).
-#
-# App's directory and MIOT must intersect at REPO_PATH, which will be determined
-# automatically if it's a Git repo, otherwise REPO_PATH must be set explicitly.
 
 MAKEFLAGS += --warn-undefined-variables
-REPO_PATH ?= $(shell git rev-parse --show-toplevel)
 APP_VERSION ?=
 APP_BUILD_ID ?=
 PYTHON ?= python
 PLATFORM ?=
 DOCKER_EXTRA ?=
 
-REPO_ABS_PATH = $(realpath $(REPO_PATH))
-MIOT_ABS_PATH = $(realpath $(MIOT_PATH))
-MIOT_REPO_SUBDIR = $(subst $(REPO_ABS_PATH),,$(MIOT_ABS_PATH))
-APP_ABS_PATH = $(CURDIR)
-APP_SUBDIR ?= $(subst $(REPO_ABS_PATH),,$(APP_ABS_PATH))
+DOCKER_APP_PATH = /app
+DOCKER_MIOT_PATH = /mongoose-iot
 
-MIOT_PLATFORM_SUBDIR = $(MIOT_REPO_SUBDIR)/fw/platforms/$(APP_PLATFORM)
-MIOT_PLATFORM_ABS_PATH = $(REPO_ABS_PATH)/$(MIOT_PLATFORM_SUBDIR)
-SDK_VERSION ?= $(shell cat $(MIOT_PLATFORM_ABS_PATH)/sdk.version)
+APP_PATH = $(CURDIR)
+MIOT_PATH_ABS=$(abspath $(MIOT_PATH))
+
+SDK_VERSION ?= $(shell cat $(MIOT_PATH)/fw/platforms/$(APP_PLATFORM)/sdk.version)
 
 # this hack is needed to make `-$(MAKEFLAGS)` always work (notice the dash).
 # Otherwise, $(MAKEFLAGS) does not contain the flag `w` when `make` runs
@@ -37,30 +31,37 @@ T=$(shell [ -t 0 ] && echo true || echo false)
 INNER_MAKE?=$(MAKE)
 
 # `make` command, which will be invoked either directly or inside the newly
-# created docker container. It uses MAKE_REPO_ABS_PATH, which will be set later
+# created docker container. It uses MAKE_APP_PATH and MAKE_MIOT_PATH which
+# will be set later.
+#
+# NOTE that $(MAKEFILES) should be given _before_ our own variable definitions,
+# because MAKEFLAGS contains MIOT_PATH which we want to override.
 MAKE_CMD=$(INNER_MAKE) -j4 \
-      -C $(MAKE_REPO_ABS_PATH)/$(APP_SUBDIR) -f Makefile.build \
+      -C $(MAKE_APP_PATH) -f Makefile.build \
+      -$(MAKEFLAGS) \
       APP=$(APP) \
       APP_VERSION=$(APP_VERSION) \
       APP_BUILD_ID=$(APP_BUILD_ID) \
-      MIOT_PATH=$(MAKE_REPO_ABS_PATH)/$(MIOT_REPO_SUBDIR) \
+      MIOT_PATH=$(MAKE_MIOT_PATH) \
       PLATFORM=$(PLATFORM) \
-      $@ -$(MAKEFLAGS)
+      $@
 
 # define targets "all" and "clean" differently, depending on whether we're
 # inside our docker container
 ifeq ("$(MIOT_SDK_REVISION)","")
 
 # We're outside of the container, so, invoke docker properly.
-# Note about mounts: we mount repo to a stable path (/src) as well as the
+# Note about mounts: we mount repo to a stable path (/app) as well as the
 # original path outside the container, whatever it may be, so that absolute path
 # references continue to work (e.g. Git submodules are known to use abs. paths).
-MAKE_REPO_ABS_PATH=/src
+MAKE_APP_PATH=$(DOCKER_APP_PATH)
+MAKE_MIOT_PATH=$(DOCKER_MIOT_PATH)
 INNER_MAKE=make
 all clean:
 	@docker run --rm -i --tty=$T \
-	  -v $(REPO_ABS_PATH):/src \
-	  -v $(REPO_ABS_PATH):$(REPO_ABS_PATH) \
+	  -v $(APP_PATH):$(DOCKER_APP_PATH) \
+	  -v $(MIOT_PATH_ABS):$(DOCKER_MIOT_PATH) \
+	  -v $(MIOT_PATH_ABS):$(MIOT_PATH_ABS) \
 	  $(DOCKER_EXTRA) $(SDK_VERSION) \
 	  /bin/bash -c "\
 	    nice $(MAKE_CMD) \
@@ -69,7 +70,8 @@ all clean:
 else
 
 # We're already inside of container, so, invoke `make` directly
-MAKE_REPO_ABS_PATH=$(REPO_ABS_PATH)
+MAKE_APP_PATH=$(APP_PATH)
+MAKE_MIOT_PATH=$(MIOT_PATH_ABS)
 all clean:
 	@$(MAKE_CMD)
 
