803c77ea172e97c8276ebf77461ac3cd67723a04
Sergey Lyubka <lsm@cesanta.com>
2016-11-21 18:46:05 +0000

    Add c_http FW example
    PUBLISHED_FROM=e0128898192277c9069f3ac5a471a27a8c318070

---
+++ fw/examples/c_http/README.md
@@ -0,0 +1,31 @@
+# This is a C-only Mongoose IoT application.
+
+
+This example shows how to control a device remotely using HTTP protocol.
+
+Assuming you have downloaded the
+[miot utility](https://mongoose-iot.com/software.html) and have registered an
+account at http://mongoose.cloud. Please follow these steps:
+
+```Bash
+git clone https://github.com/cesanta/mongoose-iot
+cd mongoose-iot/fw/examples/c_http
+export MIOT_PORT=/dev/ttyUSB0  # or, whatever is correct on your system
+export MIOT_USER=YOUR_USER_NAME
+export MIOT_PASS=YOUR_PASSWORD
+miot build --arch ARCHITECTURE # cc3200 or esp8266
+miot flash
+miot miot config-set wifi.ap.enable=false wifi.sta.enable=true \
+	wifi.sta.ssid=YOUR_WIFI_NETWORK wifi.sta.pass=YOUR_WIFI_PASSWORD
+miot console
+```
+
+The `miot console` command attaches to the device's UART and prints
+log messages. Notice the IP address assigned to the module.
+
+In a separate terminal, toggle a GPIO pin. For example, on NodeMCU pin
+number 2 is wired to the built-in blue LED. Turn it on:
+
+```
+curl -d '{pin:2 state: 0}' http://IP_ADDRESS_/ctl
+```
---
+++ fw/examples/c_http/src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "common/cs_dbg.h"
+#include "common/platform.h"
+#include "frozen/frozen.h"
+#include "fw/src/miot_app.h"
+#include "fw/src/miot_gpio.h"
+#include "fw/src/miot_mongoose.h"
+
+static void ctl_handler(struct mg_connection *c, int ev, void *p) {
+  if (ev == MG_EV_HTTP_REQUEST) {
+    struct http_message *hm = (struct http_message *) p;
+    struct mg_str *s = hm->body.len > 0 ? &hm->body : &hm->query_string;
+
+    int pin, state, status = -1;
+    if (json_scanf(s->p, s->len, "{pin: %d, state: %d}", &pin, &state) == 2) {
+      miot_gpio_set_mode(pin, GPIO_MODE_OUTPUT, GPIO_PULL_FLOAT);
+      miot_gpio_write(pin, state > 0 ? GPIO_LEVEL_HIGH : GPIO_LEVEL_LOW);
+      status = 0;
+    }
+    mg_printf(c, "HTTP/1.0 200 OK\n\n{\"status\": %d}\n", status);
+    c->flags |= MG_F_SEND_AND_CLOSE;
+    LOG(LL_INFO, ("Got: [%.*s]", (int) s->len, s->p));
+  }
+}
+
+enum miot_app_init_result miot_app_init(void) {
+  mg_register_http_endpoint(miot_get_http_listening_conn(), "/ctl",
+                            ctl_handler);
+  return MIOT_APP_INIT_SUCCESS;
+}
