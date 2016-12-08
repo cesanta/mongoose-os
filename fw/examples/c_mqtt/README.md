b189776f06464b06869d6ce8c243fa427ef7df1d
Sergey Lyubka <lsm@cesanta.com>
2016-12-08 15:03:06 +0000

    c_mqtt: more generic device control
    PUBLISHED_FROM=27bb56397fe5314793635790a67acc5e447a7feb

--- fw/examples/c_mqtt/README.md
+++ fw/examples/c_mqtt/README.md
@@ -11,8 +11,11 @@ Then you're ready to onboard
 ```
 miot build
 miot flash
-miot aws-iot-setup
+miot aws-iot-setup --aws-iot-policy YOUR_POLICY
+miot config-set mqtt.pub=aa mqtt.sub=bb
+miot config-set wifi.ap.enable=false wifi.sta.enable=true
+miot config-set wifi.sta.ssid=YOUR_WIFI_NET wifi.sta.pass=YOUR_WIFI_PASSWORD
+miot console
 ```
 
-Login to AWS IoT console, send `{pin: 2, state: 0}` message to the device's
-topic.
+Login to AWS IoT console, send `{pin: 2, state: 0}` message to MQTT topic `bb`.
--- fw/examples/c_mqtt/src/main.c
+++ fw/examples/c_mqtt/src/main.c
@@ -3,17 +3,10 @@
 #include "common/platform.h"
 #include "fw/src/miot_app.h"
 #include "fw/src/miot_gpio.h"
+#include "fw/src/miot_i2c.h"
 #include "fw/src/miot_mqtt.h"
 #include "fw/src/miot_sys_config.h"
 #include "fw/src/miot_wifi.h"
-
-#if CS_PLATFORM == CS_P_ESP8266
-#define BUTTON_PIN 0 /* GPIO0, "Flash" button on NodeMCU */
-#elif CS_PLATFORM == CS_P_CC3200
-#define BUTTON_PIN 15 /* SW2 on LAUNCHXL */
-#else
-#error Unknown platform
-#endif
 
 static void sub(struct mg_connection *c, const char *fmt, ...) {
   char buf[100];
@@ -26,24 +19,86 @@ static void sub(struct mg_connection *c, const char *fmt, ...) {
   LOG(LL_INFO, ("Subscribed to %s", buf));
 }
 
+static void pub(struct mg_connection *c, const char *fmt, ...) {
+  char msg[50];
+  struct json_out jmo = JSON_OUT_BUF(msg, sizeof(msg));
+  va_list ap;
+  int n;
+  va_start(ap, fmt);
+  n = json_vprintf(&jmo, fmt, ap);
+  va_end(ap);
+  mg_mqtt_publish(c, get_cfg()->mqtt.pub, 123, MG_MQTT_QOS(0), msg, n);
+  LOG(LL_INFO, ("%s -> %s", get_cfg()->mqtt.pub, msg));
+}
+
+static uint8_t from_hex(const char *s) {
+#define HEXTOI(x) (x >= '0' && x <= '9' ? x - '0' : x - 'W')
+  int a = tolower(*(const unsigned char *) s);
+  int b = tolower(*(const unsigned char *) (s + 1));
+  return (HEXTOI(a) << 4) | HEXTOI(b);
+}
+
+void gpio_int_handler(int pin, enum gpio_level level, void *arg) {
+  static double last = 0;
+  double now = mg_time();
+  if (now - last > 0.2) {
+    struct mg_connection *c = miot_mqtt_get_global_conn();
+    last = now;
+    if (c != NULL) {
+      pub(c, "{type: %Q, pin: %d}", "click", pin);
+    }
+    LOG(LL_INFO, ("Click!"));
+  }
+  miot_gpio_intr_set(pin, GPIO_INTR_POSEDGE);
+  (void) level;
+  (void) arg;
+}
+
 static void ev_handler(struct mg_connection *c, int ev, void *p) {
   struct mg_mqtt_message *msg = (struct mg_mqtt_message *) p;
 
   if (ev == MG_EV_MQTT_CONNACK) {
-    /* Connected to the cloud, subscribe to the control topic */
     LOG(LL_INFO, ("CONNACK: %d", msg->connack_ret_code));
-    sub(c, "/%s/gpio", get_cfg()->device.id);
+    if (get_cfg()->mqtt.sub == NULL) {
+      LOG(LL_INFO, ("No topic is set! Run 'miot config-set mqtt.sub=...'"));
+    } else {
+      sub(c, "%s", get_cfg()->mqtt.sub);
+    }
   } else if (ev == MG_EV_MQTT_PUBLISH) {
-    /* Received control command - parse it and apply the logic */
     struct mg_str *s = &msg->payload;
     int pin, state;
-    if (json_scanf(s->p, s->len, "{pin: %d, state: %d}", &pin, &state) == 2) {
+    char *data = NULL;
+
+    if (json_scanf(s->p, s->len, "{gpio: {pin: %d, state: %d}}", &pin,
+                   &state) == 2) {
+      /* Set GPIO pin to a given state */
       miot_gpio_set_mode(pin, GPIO_MODE_OUTPUT, GPIO_PULL_FLOAT);
       miot_gpio_write(pin, state > 0 ? GPIO_LEVEL_HIGH : GPIO_LEVEL_LOW);
-      LOG(LL_INFO, ("Done: [%.*s]", (int) s->len, s->p));
+      pub(c, "{type: %Q, pin: %d, state: %d}", "gpio", pin, state);
+    } else if (json_scanf(s->p, s->len, "{button: {pin: %d}}", &pin) == 1) {
+      /* Report button press on GPIO pin to a publish topic */
+      miot_gpio_intr_init(gpio_int_handler, NULL);
+      miot_gpio_intr_set(pin, GPIO_INTR_POSEDGE);
+      pub(c, "{type: %Q, pin: %d}", "button", pin);
+    } else if (json_scanf(s->p, s->len, "{i2c_write: {data: %Q}}", &data) ==
+               1) {
+      /* Write byte sequence to I2C. First byte is an address */
+      struct miot_i2c *i2c = miot_i2c_get_global();
+      if (i2c == NULL) {
+        LOG(LL_ERROR, ("I2C is not enabled!"));
+      } else {
+        enum i2c_ack_type ret = miot_i2c_start(i2c, from_hex(data), I2C_WRITE);
+        for (size_t i = 2; i < strlen(data) && ret == I2C_ACK; i += 2) {
+          ret = miot_i2c_send_byte(i2c, from_hex(data + i));
+          LOG(LL_DEBUG, ("i2c -> %02x", from_hex(data + i)));
+        }
+        miot_i2c_stop(i2c);
+        pub(c, "{type: %Q, result: %d}", "i2c", ret);
+      }
     } else {
       LOG(LL_ERROR, ("Unknown command: [%.*s]", (int) s->len, s->p));
     }
+    free(data);
   }
 }
 
@@ -54,31 +109,7 @@ static void on_wifi_event(enum miot_wifi_status ev, void *data) {
   (void) data;
 }
 
-void gpio_int_handler(int pin, enum gpio_level level, void *arg) {
-  static double last = 0;
-  double now = mg_time();
-  if (now - last > 0.2) {
-    struct mg_connection *nc = miot_mqtt_get_global_conn();
-    last = now;
-    if (nc != NULL) {
-      char message[50], topic[50];
-      struct json_out jmo = JSON_OUT_BUF(message, sizeof(message));
-      int n = json_printf(&jmo, "{pin: %d}", pin);
-      snprintf(topic, sizeof(topic), "/%s/button", get_cfg()->device.id);
-      mg_mqtt_publish(nc, topic, 123, MG_MQTT_QOS(0), message, n);
-      LOG(LL_INFO, ("Click! Published %s to %s.", message, topic));
-    } else {
-      LOG(LL_INFO, ("Click!"));
-    }
-  }
-  miot_gpio_intr_set(BUTTON_PIN, GPIO_INTR_POSEDGE);
-  (void) level;
-  (void) arg;
-}
-
 enum miot_app_init_result miot_app_init(void) {
   miot_wifi_add_on_change_cb(on_wifi_event, 0);
-  miot_gpio_intr_init(gpio_int_handler, NULL);
-  miot_gpio_intr_set(BUTTON_PIN, GPIO_INTR_POSEDGE);
   return MIOT_APP_INIT_SUCCESS;
 }
