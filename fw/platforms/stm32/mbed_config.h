40aea4cdbfb71a8518c28b0f1dc611caa524da48
Alexander Alashkin <alexander.alashkin@cesanta.com>
2016-12-16 21:45:14 +0200

    MBED missed bits
    PUBLISHED_FROM=35a596527009f5e5927d7f3ac505afd485d41f66

---
+++ fw/platforms/stm32/mbed_config.h
@@ -0,0 +1,19 @@
+// Automatically generated configuration file.
+// DO NOT EDIT, content will be overwritten.
+
+#ifndef __MBED_CONFIG_DATA__
+#define __MBED_CONFIG_DATA__
+
+// Configuration parameters
+#define MBED_CONF_EVENTS_PRESENT                    1    // set by library:events
+#define MBED_CONF_RTOS_PRESENT                      1    // set by library:rtos
+#define MBED_CONF_PLATFORM_STDIO_BAUD_RATE          9600 // set by library:platform
+#define MBED_CONF_NSAPI_PRESENT                     1    // set by library:nsapi
+#define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE 9600 // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT      1    // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES   0    // set by library:platform
+// Macros
+#define UNITY_INCLUDE_CONFIG_H                           // defined by library:utest
+
+
+#endif
