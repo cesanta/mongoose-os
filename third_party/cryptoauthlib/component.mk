38340336fceb673449a55c3e89c5ef72ddb16023
Deomid Ryabkov <rojer@cesanta.com>
2016-12-22 12:02:54 +0000

    ESP32 component makefile for CryptoAuthLib
    PUBLISHED_FROM=b5c5ee28cc0b125240b07f09904279f392b88bc2

---
+++ third_party/cryptoauthlib/component.mk
@@ -0,0 +1,8 @@
+#
+# ESP32 SDK build system component Makefile
+#
+
+COMPONENT_ADD_INCLUDEDIRS := port/include include
+
+COMPONENT_SRCDIRS := lib lib/basic lib/host
+COMPONENT_ADD_INCLUDEDIRS := lib lib/hal
