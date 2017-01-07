500284377b7a1060fdd5cf8f9c8052b132521d86
Sergey Lyubka <lsm@cesanta.com>
2017-01-07 10:45:05 +0200

    Refactor mjs_hello example
    PUBLISHED_FROM=2548ecf628055339b23ebb6429b48e2494e550dc

--- fw/examples/mjs_hello/fs/init.js
+++ fw/examples/mjs_hello/fs/init.js
@@ -1,21 +1,21 @@
-let gpio_set_mode = ffi('void mgos_gpio_set_mode(int,int)');
-let gpio_toggle = ffi('void mgos_gpio_toggle(int)');
-let gpio_read = ffi('void mgos_gpio_read(int)');
-let timer_set = ffi('int mgos_set_timer(int,int,void(*)(userdata),userdata)');
-let get_pin = ffi('int get_led_gpio_pin()');
+let GPIO = {
+  set_mode: ffi('void mgos_gpio_set_mode(int,int)'),
+  toggle: ffi('void mgos_gpio_toggle(int)'),
+  read: ffi('void mgos_gpio_read(int)')
+};
 
+let Timer = {
+  set: ffi('int mgos_set_timer(int,int,void(*)(userdata),userdata)')
+};
+
+let callback = function(pin) {
+  GPIO.toggle(pin);
+  print(GPIO.read(pin) ? 'Tick' : 'Tock');
+};
 
 // On NodeMCU, pin 2 is a built-in blue LED
 // On CC3200 Launchpad, use pin 64 (a built-in red LED)
-let PIN = get_pin();
-let callback = function(pin) {
-  gpio_toggle(pin);
-  if (gpio_read(pin)) {
-    print('Tick');
-  } else {
-    print('Tock');
-  }
-};
+let PIN = ffi('int get_led_gpio_pin()')();
 
-gpio_set_mode(PIN, 1);
-timer_set(500 /* milliseconds */, 1 /* repeat */, callback, PIN);
+GPIO.set_mode(PIN, 1);
+Timer.set(1000 /* milliseconds */, 1 /* repeat */, callback, PIN);
