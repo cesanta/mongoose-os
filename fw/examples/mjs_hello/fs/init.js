a89904fd0ed6dfbe4da2324c881d0cb88cb3e399
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-01-12 18:42:40 +0000

    Fix MJS example
    PUBLISHED_FROM=b174f31cfd507cb4ba69260a3511d1f3d10cbdbf

--- fw/examples/mjs_hello/fs/init.js
+++ fw/examples/mjs_hello/fs/init.js
@@ -7,15 +7,6 @@ let PIN = ffi('int get_led_gpio_pin()')();  // Helper C function that returns a
                                             // built-in LED GPIO
 GPIO.set_mode(PIN, GPIO.MODE_OUTPUT);
 Timer.set(1000 /* milliseconds */, 1 /* repeat */, function(pin) {
-  GPIO.toggle(pin);
-  print(GPIO.read(pin) ? 'Tick' : 'Tock');
+  let value = GPIO.toggle(pin);
+  print(value ? 'Tick' : 'Tock');
 }, PIN);
-
-// Print a message on a button press
-let pin2 = 0;
-GPIO.set_mode(pin2, GPIO.MODE_INPUT);
-GPIO.set_pull(pin2, GPIO.PULL_UP);
-GPIO.set_int_handler(pin2, GPIO.INT_EDGE_NEG, function(pin) {
-  print('Button press, pin: ', pin);
-}, true);
-GPIO.enable_int(pin2);  // Must be called after GPIO.set_int_handler()
