324a0ceee6b2e80be07bc1c6b32c002bb6533d0e
Dmitry Frank <mail@dmitryfrank.com>
2017-01-24 01:22:44 +0200

    Implement segmented stack module
    The plan is to use it at least in brainfarth.
    
    PUBLISHED_FROM=8362fa9ba3bede0b972cbf5b1739c56121e1a9fe

---
+++ common/segstack/segstack.c
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#include "segstack.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+ * Returns an index of the segment for the given cell index. If negative
+ * cellidx is given, -1 is retured.
+ */
+#define SS_SEG_IDX(ss, cellidx) \
+  ((cellidx) >= 0 ? ((cellidx) / (ss)->opt.seg_size) : -1)
+
+/*
+ * Returns an index of the cell inside of the segment.
+ */
+#define SS_SEG_CELL_IDX(ss, cellidx) ((cellidx) % (ss)->opt.seg_size)
+
+/*
+ * Returns the size of a segment in bytes.
+ */
+#define SS_SEG_SIZE_BYTES(ss) \
+  (sizeof(struct segstack_seg) + (ss)->opt.cell_size * (ss)->opt.seg_size)
+
+static void *s_segstack_get_cellp(struct segstack *ss, int cellidx,
+                                  struct segstack_seg **pseg);
+static void s_segstack_free_last_seg(struct segstack *ss);
+
+void segstack_init(struct segstack *ss, const struct segstack_opt *opt) {
+  memset(ss, 0, sizeof(*ss));
+  memcpy(&ss->opt, opt, sizeof(ss->opt));
+  /* TODO(dfrank): check the validity of the options */
+}
+
+void segstack_free(struct segstack *ss) {
+  while (ss->last_seg != NULL) {
+    s_segstack_free_last_seg(ss);
+  }
+  memset(ss, 0, sizeof(*ss));
+}
+
+int segstack_size(struct segstack *ss) {
+  return ss->size;
+}
+
+void *segstack_tos(struct segstack *ss) {
+  return segstack_at(ss, -1);
+}
+
+void *segstack_at(struct segstack *ss, int idx) {
+  if (idx < 0) {
+    idx = ss->size + idx;
+  }
+
+  if (idx < 0 || idx >= ss->size) {
+    return NULL;
+  }
+
+  return s_segstack_get_cellp(ss, idx, NULL);
+}
+
+void segstack_push(struct segstack *ss, const void *cellp) {
+  void *tgt_cellp = s_segstack_get_cellp(ss, ss->size, &ss->tos_seg);
+  ss->size++;
+  memcpy((char *) tgt_cellp, (char *) cellp, ss->opt.cell_size);
+}
+
+void segstack_pop(struct segstack *ss, void *cellp) {
+  assert(ss->size > 0);
+  struct segstack_seg *old_tos_seg = ss->tos_seg;
+
+  /* If the caller provided a pointer to write the popped value to, do it */
+  if (cellp != NULL) {
+    memcpy(cellp, segstack_tos(ss), ss->opt.cell_size);
+  }
+
+  /* Pop the top element from the stack */
+  s_segstack_get_cellp(ss, ss->size - 2, &ss->tos_seg);
+  ss->size--;
+
+  if (old_tos_seg != ss->tos_seg) {
+    /* A new segment just became unused */
+    assert(old_tos_seg->prev == ss->tos_seg);
+
+    if (ss->stashed_segs < ss->opt.stash_segs) {
+      /* We should stash this unused segment */
+      ss->stashed_segs++;
+    } else {
+      /* No more segments should be stashed, so, free the last one */
+      s_segstack_free_last_seg(ss);
+    }
+  }
+}
+
+/*
+ * Returns segment by the TOS delta. If `tos_delta` is 0, returns the TOS
+ * segment.  If `tos_delta > 0`, returns one of the previous segments.  If
+ * `tos_delta` is equal to -1, returns the next segment: eigher previously
+ * stashed, or newly allocated.
+ *
+ * Other values of `tos_delta` are illegal.
+ */
+static struct segstack_seg *s_segstack_get_seg(struct segstack *ss,
+                                               int tos_delta) {
+  struct segstack_seg *cur_seg = NULL;
+  if (tos_delta >= 0) {
+    /* We need to get TOS segment or below */
+    cur_seg = ss->tos_seg;
+    while (tos_delta-- > 0) {
+      cur_seg = cur_seg->prev;
+    }
+  } else if (tos_delta == -1) {
+    /* We need to get the "next" segment after TOS */
+    if (ss->stashed_segs > 0) {
+      /* The needed segment is already allocated (stashed), we need to find it
+       */
+      assert(ss->last_seg != NULL);
+      assert(ss->last_seg != ss->tos_seg);
+
+      cur_seg = ss->last_seg;
+      while (cur_seg->prev != ss->tos_seg) {
+        cur_seg = cur_seg->prev;
+        assert(cur_seg != NULL);
+      }
+
+      ss->stashed_segs--;
+    } else {
+      /* We need to allocate a new segment */
+      ss->last_seg = (struct segstack_seg *) calloc(1, SS_SEG_SIZE_BYTES(ss));
+      ss->last_seg->prev = ss->tos_seg;
+      cur_seg = ss->last_seg;
+    }
+  } else {
+    abort();
+  }
+  return cur_seg;
+}
+
+/*
+ * Returns a pointer to the cell by the given cell index. If `pseg` is not NULL,
+ * a pointer to a segment which contains the cell will be written there.
+ *
+ * Cell index might be in range [-1, ss->size].
+ *
+ * If cellidx is -1, NULL is returned.
+ *
+ * If cellidx is equal to `ss->size`, a pointer to the next cell is returned
+ * (probably from stashed or newly allocated segment).
+ */
+static void *s_segstack_get_cellp(struct segstack *ss, int cellidx,
+                                  struct segstack_seg **pseg) {
+  int tos_seg_idx = SS_SEG_IDX(ss, ss->size - 1);
+  int need_seg_idx = SS_SEG_IDX(ss, cellidx);
+  int need_seg_cell_idx = SS_SEG_CELL_IDX(ss, cellidx);
+
+  struct segstack_seg *seg = NULL;
+
+  assert(cellidx >= -1 && cellidx <= ss->size);
+
+  seg = s_segstack_get_seg(ss, tos_seg_idx - need_seg_idx);
+
+  if (pseg != NULL) {
+    *pseg = seg;
+  }
+
+  if (seg != NULL) {
+    return seg->data + ss->opt.cell_size * need_seg_cell_idx;
+  } else {
+    return NULL;
+  }
+}
+
+/*
+ * Frees last segment.
+ */
+static void s_segstack_free_last_seg(struct segstack *ss) {
+  assert(ss->last_seg != NULL);
+  struct segstack_seg *t = ss->last_seg;
+  ss->last_seg = ss->last_seg->prev;
+  free(t);
+}
---
+++ common/segstack/unit_test.c
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ *
+ * This library is dual-licensed: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation. For the terms of this
+ * license, see <http: *www.gnu.org/licenses/>.
+ *
+ * You are free to use this library under the terms of the GNU General
+ * Public License, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * Alternatively, you can license this library under a commercial
+ * license, as set out in <http://cesanta.com/products.html>.
+ */
+
+/*
+ * To unit test on Mac system, do
+ *
+ * g++ unit_test.c -o unit_test -W -Wall -g -O0 -fprofile-arcs -ftest-coverage
+ * clang unit_test.c -o unit_test -W -Wall -g -O0 -fprofile-arcs -ftest-coverage
+ * ./unit_test
+ * gcov -a unit_test.c
+ */
+
+#include "segstack.c"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FAIL(str, line)                           \
+  do {                                            \
+    printf("Fail on line %d: [%s]\n", line, str); \
+    return str;                                   \
+  } while (0)
+
+#define ASSERT(expr)                    \
+  do {                                  \
+    static_num_tests++;                 \
+    if (!(expr)) FAIL(#expr, __LINE__); \
+  } while (0)
+
+/* VC6 doesn't know how to cast an unsigned 64-bit int to double */
+#if (defined(_MSC_VER) && _MSC_VER <= 1200)
+#define AS_DOUBLE(d) (double)(int64_t)(d)
+#else
+#define AS_DOUBLE(d) (double)(d)
+#endif
+
+/*
+ * Numeric equality assertion. Comparison is made in native types but for
+ * printing both are convetrted to double.
+ */
+#define ASSERT_EQ(actual, expected)                                 \
+  do {                                                              \
+    static_num_tests++;                                             \
+    if (!((actual) == (expected))) {                                \
+      printf("%f != %f\n", AS_DOUBLE(actual), AS_DOUBLE(expected)); \
+      FAIL(#actual " == " #expected, __LINE__);                     \
+    }                                                               \
+  } while (0)
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define RUN_TEST(test)        \
+  do {                        \
+    const char *msg = test(); \
+    if (msg) return msg;      \
+  } while (0)
+
+static int static_num_tests = 0;
+
+static int num_segs(struct segstack *ss) {
+  int ret = 0;
+  struct segstack_seg *bp = ss->last_seg;
+  while (bp != NULL) {
+    ret++;
+    bp = bp->prev;
+  }
+  return ret;
+}
+
+static int64_t f(int i) {
+  return i * 2 + 100;
+}
+
+static const char *test_pop(struct segstack *ss, int i, int seg_size,
+                            int max_segs_num, int stash_size) {
+  /* Check segs number */
+  int expected_segs_num = (i / seg_size) + 1 + stash_size;
+  if (expected_segs_num > max_segs_num) {
+    expected_segs_num = max_segs_num;
+  }
+  ASSERT_EQ(num_segs(ss), expected_segs_num);
+
+  /* Check TOS */
+  int32_t val = *((int32_t *) segstack_tos(ss));
+  ASSERT_EQ(val, f(i));
+
+  /* Check popped value (should be the same as the TOS value above) */
+  int32_t val2 = 0;
+  segstack_pop(ss, &val2);
+  ASSERT_EQ(val2, f(i));
+
+  return NULL;
+}
+
+static const char *test_general(void) {
+  uint8_t seg_size;
+  uint8_t stash_size;
+
+  /* Try various stash sizes... */
+  for (stash_size = 0; stash_size <= 3; stash_size++) {
+    /* And various seg sizes */
+    for (seg_size = 1; seg_size <= 16; seg_size <<= 1) {
+      struct segstack ss;
+      struct segstack_opt opt = {
+          .cell_size = 4, .seg_size = seg_size, .stash_segs = stash_size,
+      };
+      const int maxsize = 500;
+      const int maxback = 50;
+      int max_segs_num = 0;
+      segstack_init(&ss, &opt);
+
+      /* Fill the stack with values */
+      int32_t i;
+      for (i = 0; i <= maxsize; i++) {
+        int32_t val = f(i);
+        segstack_push(&ss, &val);
+        ASSERT_EQ(num_segs(&ss), (i / seg_size + 1));
+      }
+
+      max_segs_num = num_segs(&ss);
+
+      ASSERT(segstack_at(&ss, maxsize + 1) == NULL);
+      ASSERT(segstack_at(&ss, -maxsize - 2) == NULL);
+
+      /* Check each element of the stack */
+      for (i = 0; i <= maxsize; i++) {
+        ASSERT_EQ(*(int32_t *) segstack_at(&ss, i), f(i));
+        ASSERT_EQ(num_segs(&ss), (maxsize / seg_size + 1));
+      }
+
+      /* Pop various amounts of cells from the stack, and push them back */
+      int back;
+      for (back = 1; back < maxback; back++) {
+        for (i = maxsize; i >= (maxsize - back); i--) {
+          const char *t = test_pop(&ss, i, seg_size, max_segs_num, stash_size);
+          if (t != NULL) {
+            return t;
+          }
+          int32_t val = 0xa5a5a5a5;
+          segstack_push(&ss, &val);
+          segstack_pop(&ss, NULL);
+        }
+
+        for (i = (maxsize - back); i <= maxsize; i++) {
+          int32_t val = f(i);
+          segstack_push(&ss, &val);
+        }
+      }
+
+      /* Pop all elements from the stack */
+      for (i = maxsize; i >= 0; i--) {
+        const char *t = test_pop(&ss, i, seg_size, max_segs_num, stash_size);
+        if (t != NULL) {
+          return t;
+        }
+      }
+
+      /* There should be no TOS, and len is 0 */
+      ASSERT_EQ(segstack_size(&ss), 0);
+      ASSERT(ss.tos_seg == NULL);
+
+      /* In case of no stash, last_seg should also be NULL */
+      if (stash_size == 0) {
+        ASSERT(ss.last_seg == NULL);
+      }
+
+      segstack_free(&ss);
+    }
+  }
+  return NULL;
+}
+
+static const char *run_all_tests(void) {
+  RUN_TEST(test_general);
+  return NULL;
+}
+
+int main(void) {
+  const char *fail_msg = run_all_tests();
+  printf("%s, tests run: %d\n", fail_msg ? "FAIL" : "PASS", static_num_tests);
+  return fail_msg == NULL ? EXIT_SUCCESS : EXIT_FAILURE;
+}
