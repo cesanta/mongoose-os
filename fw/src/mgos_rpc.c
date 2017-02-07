4373fb3fb41e3441877fdc9baabc8c5ed519b50e
Deomid Ryabkov <rojer@cesanta.com>
2017-02-07 10:22:44 +0200

    Add MQTT channel as default (for real this time)
    What cesanta/dev#6442 was meant to be.
    
    PUBLISHED_FROM=9d7f5a31ed5f4c5168d3c3a1d6d4d7340465e621

--- fw/src/mgos_rpc.c
+++ fw/src/mgos_rpc.c
@@ -160,8 +160,8 @@ enum mgos_init_result mgos_rpc_init(void) {
     struct mg_rpc_channel *mch =
         mg_rpc_channel_mqtt(mg_mk_str(get_cfg()->device.id));
     if (mch == NULL) return MGOS_INIT_MG_RPC_FAILED;
-    mg_rpc_add_channel(c, mg_mk_str(""), mch, sccfg->mqtt.is_trusted,
-                       false /* send_hello */);
+    mg_rpc_add_channel(c, mg_mk_str(MG_RPC_DST_DEFAULT), mch,
+                       sccfg->mqtt.is_trusted, false /* send_hello */);
   }
 #endif
 
@@ -175,8 +175,8 @@ enum mgos_init_result mgos_rpc_init(void) {
     if (mgos_uart_init(scucfg->uart_no, ucfg, NULL, NULL) != NULL) {
       struct mg_rpc_channel *uch =
           mg_rpc_channel_uart(scucfg->uart_no, scucfg->wait_for_start_frame);
-      mg_rpc_add_channel(c, mg_mk_str(MG_RPC_DST_DEFAULT), uch,
-                         true /* is_trusted */, false /* send_hello */);
+      mg_rpc_add_channel(c, mg_mk_str(""), uch, true /* is_trusted */,
+                         false /* send_hello */);
       uch->ch_connect(uch);
     } else {
       LOG(LL_ERROR, ("UART%d init failed", scucfg->uart_no));
