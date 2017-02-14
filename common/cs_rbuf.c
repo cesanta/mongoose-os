d814b95058d2b0b57ffd1821771395a68e03cb53
Deomid Ryabkov <rojer@cesanta.com>
2017-02-14 23:14:55 +0800

    Move cs_rbuf out of IRAM; no longer required
    PUBLISHED_FROM=cc6e93157ef8a4f082653ed14feb27a9fde085e3

--- common/cs_rbuf.c
+++ common/cs_rbuf.c
@@ -8,31 +8,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef IRAM
-#define IRAM
-#endif
-
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-IRAM void cs_rbuf_init(cs_rbuf_t *b, uint16_t size) {
+void cs_rbuf_init(cs_rbuf_t *b, uint16_t size) {
   b->begin = calloc(1, size);
   b->size = size;
   cs_rbuf_clear(b);
 }
 
-IRAM void cs_rbuf_deinit(cs_rbuf_t *b) {
+void cs_rbuf_deinit(cs_rbuf_t *b) {
   free(b->begin);
   memset(b, 0, sizeof(*b));
 }
 
-IRAM void cs_rbuf_clear(cs_rbuf_t *b) {
+void cs_rbuf_clear(cs_rbuf_t *b) {
   b->used = b->in_flight = 0;
   b->size = b->avail = b->size;
   b->end = b->begin + b->size;
   b->head = b->tail = b->begin;
 }
 
-IRAM void cs_rbuf_append(cs_rbuf_t *b, const void *data, uint16_t len) {
+void cs_rbuf_append(cs_rbuf_t *b, const void *data, uint16_t len) {
   uint8_t *p = (uint8_t *) data;
   b->used += len;
   b->avail -= len;
@@ -42,20 +38,20 @@ IRAM void cs_rbuf_append(cs_rbuf_t *b, const void *data, uint16_t len) {
   }
 }
 
-IRAM void cs_rbuf_append_one(cs_rbuf_t *b, uint8_t byte) {
+void cs_rbuf_append_one(cs_rbuf_t *b, uint8_t byte) {
   *b->tail++ = byte;
   if (b->tail >= b->end) b->tail = b->begin;
   b->used++;
   b->avail--;
 }
 
-IRAM uint8_t cs_rbuf_at(cs_rbuf_t *b, uint16_t i) {
+uint8_t cs_rbuf_at(cs_rbuf_t *b, uint16_t i) {
   uint8_t *p = b->head + i;
   if (p >= b->end) p = b->begin + (p - b->end);
   return *p;
 }
 
-IRAM uint16_t cs_rbuf_get(cs_rbuf_t *b, uint16_t max, uint8_t **data) {
+uint16_t cs_rbuf_get(cs_rbuf_t *b, uint16_t max, uint8_t **data) {
   uint8_t *start = b->head + b->in_flight;
   if (start >= b->end) start = b->begin + (start - b->end);
   *data = start;
@@ -66,7 +62,7 @@ IRAM uint16_t cs_rbuf_get(cs_rbuf_t *b, uint16_t max, uint8_t **data) {
   return len;
 }
 
-IRAM void cs_rbuf_consume(cs_rbuf_t *b, uint16_t len) {
+void cs_rbuf_consume(cs_rbuf_t *b, uint16_t len) {
   b->head += len;
   if (b->head >= b->end) b->head = b->begin + (b->head - b->end);
   b->used -= len;
@@ -75,7 +71,7 @@ IRAM void cs_rbuf_consume(cs_rbuf_t *b, uint16_t len) {
   if (b->used == 0) b->head = b->tail = b->begin;
 }
 
-IRAM uint16_t cs_rbuf_contig_tail_space(cs_rbuf_t *b, uint8_t **data) {
+uint16_t cs_rbuf_contig_tail_space(cs_rbuf_t *b, uint8_t **data) {
   *data = b->tail;
   return (b->tail > b->head || b->used == 0 ? b->end - b->tail
                                             : b->head - b->tail);
