1d89d04d1bcfdb803ba9bbb73b849b50297ec369
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-02-21 19:46:19 +0200

    Add grove to the list of examples
    PUBLISHED_FROM=5aa39c82dd916155a9b413853c573b852fd7f4af

--- fw/examples/mjs_base/examples/grove.js
+++ fw/examples/mjs_base/examples/grove.js
@@ -1,5 +1,6 @@
 // This examples demonstrates how to use such Goove Kit components as
 // Button, Motion Sensor and Temperature Sensor
+// See https://www.seeedstudio.com/Grove-Indoor-Environment-Kit-for-Intel%C2%AE-Edison-p-2427.html
 
 // Load Mongoose OS API
 load('api_grove.js');
