c5912b8f53e6ac9c47f63ddfeb895b19a29ccff8
Alexander Alashkin <alexander.alashkin@cesanta.com>
2017-03-01 09:49:07 +0000

    Add Grove Relay support
    PUBLISHED_FROM=4d03bac86b9b56f408835d2d8499eee634b8ba69

--- fw/examples/mjs_base/examples/grove.js
+++ fw/examples/mjs_base/examples/grove.js
@@ -1,5 +1,5 @@
 // This examples demonstrates how to use such Goove Kit components as
-// Button, Motion Sensor, Temperature Sensor and UV Sensor
+// Button, Motion Sensor, Temperature Sensor, UV Sensor and Relay
 
 // Load Mongoose OS API
 load('api_grove.js');
@@ -10,11 +10,19 @@ let motionSensorGPIO = 26;
 let lightSensorGPIO = 36;
 let moistureSensorGPIO = 35;
 let uvSensorGPIO = 39;
+let relayGPIO = 25;
 
 Grove.Button.attach(buttonGPIO, function() {
   print("Light sensor data:", Grove.LightSensor.get(lightSensorGPIO));
   print("Moisture sensor data:", Grove.MoistureSensor.get(moistureSensorGPIO));
   print("UV sensor data:", Grove.UVSensor.get(uvSensorGPIO));
+  if (Grove.Relay.isClosed(relayGPIO) === 1) {
+    Grove.Relay.open(relayGPIO);
+    print("Relay state is now open");
+  } else {
+    Grove.Relay.close(relayGPIO);
+    print("Relay state is now closed");
+  }
 });
 
 Grove.MotionSensor.attach(motionSensorGPIO, function() {
--- fw/examples/mjs_base/fs/api_grove.js
+++ fw/examples/mjs_base/fs/api_grove.js
@@ -35,5 +35,29 @@ let Grove = {
     get: function(pin) {
       return ADC.read(pin);
     },
-  }
+  },
+  _relayInited: undefined,
+  _relayClosed: 0,
+  Relay: {
+    _init: function(pin) {
+      if (Grove._relayInited !== 1) {
+        GPIO.set_mode(pin, GPIO.MODE_OUTPUT);
+        GPIO.set_pull(pin, GPIO.PULL_DOWN);
+        Grove._relayInited = 1;
+      }
+    },
+    close: function(pin) {
+      this._init(pin);
+      GPIO.write(pin, 1);
+      Grove._relayClosed = 1;
+    },
+    open: function(pin) {
+      this._init(pin);
+      GPIO.write(pin, 0);
+      Grove._relayClosed = 0;
+    },
+    isClosed: function(pin) {
+      return Grove._relayClosed;
+    },
+  },
 };
