ac451da1912729c0367503aaab0070b2e7f8b29c
Alexander Alashkin <alexander.alashkin@cesanta.com>
2017-03-02 11:37:40 +0000

    Print mJS memory stat in mjs_base test
    PUBLISHED_FROM=53b53348d5d58003df58e792bc93c50aeba97553

--- fw/examples/mjs_base/src/main.c
+++ fw/examples/mjs_base/src/main.c
@@ -32,12 +32,18 @@ int get_led_gpio_pin(void) {
 
 enum mgos_app_init_result mgos_app_init(void) {
   /* Initialize JavaScript engine */
+  int mem1, mem2, mem3;
+  mem1 = mgos_get_free_heap_size();
   struct mjs *mjs = mjs_create();
+  mem2 = mgos_get_free_heap_size();
   mjs_set_ffi_resolver(mjs, mgos_dlsym);
   mjs_err_t err = mjs_exec_file(mjs, "init.js", NULL);
   if (err != MJS_OK) {
     LOG(LL_ERROR, ("MJS exec error: %s\n", mjs_strerror(mjs, err)));
   }
-
+  mem3 = mgos_get_free_heap_size();
+  LOG(LL_INFO, ("mJS memory stat: before init: %d "
+                "after init: %d after init.js: %d",
+                mem1, mem2, mem3));
   return MGOS_APP_INIT_SUCCESS;
 }
