bd9b04e941cf51a25fcd572e1e4a08769ed5a577
Deomid Ryabkov <rojer@cesanta.com>
2017-03-21 00:27:43 +0200

    ESP8266: change output drive from OD to direct
    PUBLISHED_FROM=489da9463993206e6a490b9a1009c118867b93d5

--- fw/platforms/esp8266/src/esp_gpio.c
+++ fw/platforms/esp8266/src/esp_gpio.c
@@ -97,9 +97,10 @@ bool mgos_gpio_set_mode(int pin, enum mgos_gpio_mode mode) {
       GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, BIT(pin));
       gpio_pin_intr_state_set(GPIO_ID_PIN(pin), GPIO_PIN_INTR_DISABLE);
       GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(pin));
+      /* Pin driver -> direct drive, pad driver -> open drain. */
       GPIO_REG_WRITE(GPIO_PIN_ADDR(GPIO_ID_PIN(pin)),
                      GPIO_REG_READ(GPIO_PIN_ADDR(GPIO_ID_PIN(pin))) &
-                         (~GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_ENABLE)));
+                         (~GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_DISABLE)));
       break;
     default:
       return false;
