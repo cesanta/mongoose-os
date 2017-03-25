6b544c5b706cf81156f7d46b3d7e839e7d86c11a
Deomid Ryabkov <rojer@cesanta.com>
2017-03-25 16:39:25 +0200

    Link with libstdc++ on ESP8266
    https://forum.cesanta.com/index.php?p=/discussion/307/gcc-g
    
    PUBLISHED_FROM=17b8e863c88372aeebe15a993eecfeae25323c24

--- fw/examples/c_hello_arduino/src/main.cpp
+++ fw/examples/c_hello_arduino/src/main.cpp
@@ -10,10 +10,24 @@ void setup(void) {
   pinMode(2, OUTPUT);
 }
 
+class TestClass {
+ public:
+  TestClass() {
+    printf("constructed\n");
+  }
+  ~TestClass() {
+    printf("destroyed %d\n", a_);
+  }
+  int a_ = 0;
+};
+
 void loop() {
   static boolean value = 0;
   digitalWrite(2, value);
   printf("%s\r\n", (value == 0 ? "Tick" : "Tock"));
   delay(500);
   value = (value ? 0 : 1);
+  TestClass *foo = new TestClass();
+  foo->a_ = 1;
+  delete (foo);
 }
