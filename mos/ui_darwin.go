4cb8cc233e344ddd944cfd987616add2458ee4ec
Aleksandr Demakin <alexander.demakin@gmail.com>
2017-03-29 11:09:21 -0700

    mos: terminate app on window close on osx

--- mos/ui_darwin.go
+++ mos/ui_darwin.go
@@ -7,6 +7,48 @@ import (
 	"github.com/alexflint/gallium"
 )
 
+/*
+#cgo CFLAGS: -x objective-c
+#cgo CFLAGS: -framework Cocoa
+#cgo LDFLAGS: -framework Cocoa
+
+#include <Cocoa/Cocoa.h>
+#include <dispatch/dispatch.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) NSWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+@synthesize window;
+
+- (id)init{
+    self = [super init];
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
+}
+
+@end
+
+void setTerminateAfterLastWindowClosed() {
+  dispatch_async(dispatch_get_main_queue(), ^{
+	[[NSApplication sharedApplication] setDelegate: [[AppDelegate alloc]init]];
+  });
+}
+*/
+import "C"
+
 func showUI(url string) {
 	err := gallium.Loop(os.Args, func(app *gallium.App) {
 		opts := gallium.FramedWindow
@@ -21,6 +63,7 @@ func showUI(url string) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		C.setTerminateAfterLastWindowClosed()
 		app.SetMenu([]gallium.Menu{
 			{
 				Title: "Mongoose OS",
