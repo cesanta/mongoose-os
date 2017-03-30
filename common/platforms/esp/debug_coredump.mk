41de0c882cd8b49c57cb70e2971428682d494c4b
Deomid Ryabkov <rojer@cesanta.com>
2017-03-30 21:24:48 +0300

    Make 'make debug_coredump' easier to use
    Example:
    ```
      [fw/platforms/esp8266]$ make debug_coredump \
          CONSOLE_LOG=/home/rojer/Downloads/cd.log \
          ELF_FILE=/home/rojer/Downloads/mos-esp8266.elf
          BIN_FILE=/home/rojer/Downloads/mjs_base-1.0/0x11000.bin
    ```
    
    PUBLISHED_FROM=b330c8f15f0fef8ce287461fa0e1ab6deb04f0c4

--- common/platforms/esp/debug_coredump.mk
+++ common/platforms/esp/debug_coredump.mk
@@ -12,14 +12,16 @@ endif
 	  -v $(APP_MOUNT_PATH):$(DOCKER_APP_PATH) \
 	  -v $(MGOS_PATH_ABS):$(DOCKER_MGOS_PATH) \
 	  -v $(MGOS_PATH_ABS):$(MGOS_PATH_ABS) \
-	  -v $(CONSOLE_LOG):/console.log \
+	  -v $(realpath $(BIN_FILE)):/app.bin \
+	  -v $(realpath $(ELF_FILE)):/app.elf \
+	  -v $(realpath $(CONSOLE_LOG)):/console.log \
 	  $(SDK_VERSION) /bin/bash -c "\
 	  cd $(DOCKER_APP_PATH)/$(APP_SUBDIR); \
 	    $(DOCKER_MGOS_PATH)/common/platforms/esp/serve_core.py \
 	      --rom $(DOCKER_MGOS_PATH)/common/platforms/$(APP_PLATFORM)/rom/rom.bin \
-	      --irom $(BIN_FILE) --irom_addr $(IROM_MAP_ADDR) \
+	      --irom /app.bin --irom_addr $(IROM_MAP_ADDR) \
 	      /console.log & \
-	    $(GDB) $(ELF_FILE) \
+	    $(GDB) /app.elf \
 	      -ex 'target remote 127.0.0.1:1234' \
 	      -ex 'set confirm off' \
 	      -ex 'add-symbol-file $(DOCKER_MGOS_PATH)/common/platforms/$(APP_PLATFORM)/rom/rom.elf 0x40000000'"
