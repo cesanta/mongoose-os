b369d94499d29872ab336a06315eabace996c9a9
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-04-05 16:38:39 +0300

    Use ace editor in the UI
    PUBLISHED_FROM=243f84b73b99271d5d6a91a69bbae4455ca1127f

--- fw/mjs_api/api_aws.js
+++ fw/mjs_api/api_aws.js
@@ -1,5 +1,33 @@
 // AWS API, see AWS documentation at
 // http://docs.aws.amazon.com/iot/latest/developerguide/iot-thing-shadows.html
+//
+// Usage example:
+//
+// ```javascript
+// load('api_aws.js');
+// load('api_gpio.js');
+//
+// let state = { on: false, counter: 0 };  // device state: shadow metadata
+//
+// // Upon startup, report current actual state, "reported"
+// // When cloud sends us a command to update state ("desired"), do it
+// AWS.Shadow.setStateHandler(function(data, event, reported, desired) {
+//   if (event === AWS.Shadow.CONNECTED) {
+//     AWS.Shadow.update(0, {state: state});  // Report current state
+//   } else if (event === AWS.Shadow.UPDATE_DELTA) {
+//     for (let key in state) {
+//       if (desired.state[key] !== undefined) state[key] = desired[key];
+//     }
+//   }
+//   print(JSON.stringify(reported), JSON.stringify(desired));
+// }, null);
+//
+// // On a button press, update press counter via the shadow
+// let buttonPin = 0;
+// GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
+//   AWS.Shadow.update(0, {desired: {on: state.on, counter: state.counter + 1}});
+// }, null);
+// ```
 
 let AWS = {
   Shadow: {
@@ -23,25 +51,6 @@ let AWS = {
     // `AWS.Shadow.UPDATE_REJECTED`, `AWS.Shadow.UPDATE_DELTA`.
     // `reported` is previously reported state object (if any), and `desired`
     // is the desired state (if present).
-    //
-    // Example:
-    // ```javascript
-    // load('api_aws.js');
-    //
-    // let state = { on: false, counter: 0 };
-    //
-    // AWS.Shadow.setStateHandler(function(data, event, reported, desired) {
-    //   print(JSON.stringify(reported), JSON.stringify(desired));
-    //   if (event === AWS.Shadow.CONNECTED) {
-    //     AWS.Shadow.update(0, {state: state});  // Report current state
-    //   } else if (event === AWS.Shadow.UPDATE_DELTA) {
-    //     // Cloud wants us to update state, apply changes
-    //     for (let key in state) {
-    //       if (desired.state[key] !== undefined) state[key] = desired[key];
-    //     }
-    //   }
-    // }, null);
-    // ```
     setStateHandler: function(cb, ud) {
       this._seth(this._scb, {
         cb: cb,
@@ -57,18 +66,6 @@ let AWS = {
     // If you want the update to be aplied only if a particular version is
     // current, specify the version. Otherwise set it to 0 to apply to any
     // version. Example: increase `state.counter` on a button press:
-    // ```javascript
-    // load('api_aws.js');
-    // load('api_gpio.js');
-    //
-    // let state = { on: false, counter: 0 };
-    // let buttonPin = 0;
-    //
-    // GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
-    //   AWS.Shadow.update(0, {desired: {on: state.on, counter: state.counter + 1}});
-    // }, null);
-    // ```
-
     update: function(ver, state) {
       return this._upd(ver, JSON.stringify(state)) === 1;
     },
