885a848c59bbf92fa534f9e12d8735a63363f551
Dmitry Frank <mail@dmitryfrank.com>
2017-04-13 22:21:37 +0100

    Fix looging of whether the channel is trusted
    PUBLISHED_FROM=77ceb4c313f15b0fafc57e87a588e8e01a57b1e0

--- common/mg_rpc/mg_rpc.c
+++ common/mg_rpc/mg_rpc.c
@@ -408,7 +408,7 @@ void mg_rpc_add_channel(struct mg_rpc *c, const struct mg_str dst,
   ch->mg_rpc_data = c;
   ch->ev_handler = mg_rpc_ev_handler;
   SLIST_INSERT_HEAD(&c->channels, ci, channels);
-  LOG(LL_DEBUG, ("%p '%.*s' %s", ch, (int) dst.len, dst.p, ch->get_type(ch),
+  LOG(LL_DEBUG, ("%p '%.*s' %s%s", ch, (int) dst.len, dst.p, ch->get_type(ch),
                  (is_trusted ? ", trusted" : "")));
 }
 
