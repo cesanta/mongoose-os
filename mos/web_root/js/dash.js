7a6b3747b3c70a75fdcecfd1110af3b974165244
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-04-17 15:14:22 +0300

    Add RPC browser
    PUBLISHED_FROM=b1b8ac3be577eaa7a86fd4981abe0b91fda8f863

--- mos/web_root/js/dash.js
+++ mos/web_root/js/dash.js
@@ -1,97 +1,119 @@
-(function($) {
-  var ws, pageCache = {};
-  PNotify.prototype.options.styling = 'fontawesome';
-  PNotify.prototype.options.delay = 5000;
-  var getCookie = function(name) {
-    var m = (document.cookie || '').match(new RegExp(name + '=([^;"\\s]+)'));
-    return m ? m[1] : '';
+var ws, pageCache = {};
+PNotify.prototype.options.styling = 'fontawesome';
+PNotify.prototype.options.delay = 5000;
+var getCookie = function(name) {
+  var m = (document.cookie || '').match(new RegExp(name + '=([^;"\\s]+)'));
+  return m ? m[1] : '';
+};
+$.ajaxSetup({type: 'POST'});
+
+$(document.body)
+    .on('click', '.dropdown-menu li', function(ev) {
+      var text = $(this).find('a').text();
+      $(this)
+          .closest('.input-group')
+          .find('input')
+          .val(text)
+          .trigger('change');
+    });
+
+$(document).ajaxSend(function(event, xhr, settings) {
+  $('#top_spinner').addClass('spinner');
+}).ajaxStop(function() {
+  $('#top_spinner').removeClass('spinner');
+}).ajaxComplete(function(event, xhr) {
+  // $('#top_nav').removeClass('spinner');
+  if (xhr.status == 200) return;
+  var errStr = xhr.responseText || 'connection errStror';
+  if (errStr.match(/deadline exceeded/)) {
+    errStr = 'Lost connection with your board. Please restart mos tool from terminal';
+  }
+  new PNotify({ title: 'Server Error', text: 'Reason: ' + errStr, type: 'error' });
+});
+
+$(document)
+    .on('click', '#reboot-button', function() {
+      $.ajax({url: '/call', data: {method: 'Sys.Reboot'}}).done(function() {
+        new PNotify({title: 'Device rebooted', type: 'success'});
+      });
+    });
+
+$(document).on('click', '#clear-logs-button', function() {
+  $('#device-logs').empty();
+  new PNotify({title: 'Console cleared', type: 'success'});
+});
+
+var connected = false;
+
+// A page which needs to be loaded once serial port connection is established
+var deferredLoadPage = undefined;
+
+var loadPage = function(page) {
+  var doit = function(html) {
+    $('#app_view').html(html);
+    $('#breadcrumb').html($('[data-title]').attr('data-title'));
   };
-  $.ajaxSetup({type: 'POST'});
+  if (pageCache[page]) {
+    doit(pageCache[page]);
+  } else {
+    $.get('page_' + page + '.html').done(function(html) {
+      pageCache[page] = html;
+      doit(html);
+    });
+  }
+};
 
-  $(document.body)
-      .on('click', '.dropdown-menu li', function(ev) {
-        var text = $(this).find('a').text();
-        $(this)
-            .closest('.input-group')
-            .find('input')
-            .val(text)
-            .trigger('change');
-      });
+$(document).on('click', 'a[tab]', function() {
+  var page = $(this).attr('tab');
+  loadPage(page);
+});
 
-  $(document).ajaxSend(function(event, xhr, settings) {
-    $('#top_spinner').addClass('spinner');
-  }).ajaxStop(function() {
-    $('#top_spinner').removeClass('spinner');
-  }).ajaxComplete(function(event, xhr) {
-    // $('#top_nav').removeClass('spinner');
-    if (xhr.status == 200) return;
-    var errStr = xhr.responseText || 'connection errStror';
-    if (errStr.match(/deadline exceeded/)) {
-      errStr = 'Lost connection with your board. Please restart mos tool from terminal';
-    }
-    new PNotify({ title: 'Server Error', text: 'Reason: ' + errStr, type: 'error' });
+$(document).ready(function() {
+  $('a[tab]').first().click();
+});
+
+$.ajax({url: '/call', data: {method: 'Sys.GetInfo'}}).then(function(data) {
+  var json = data.result;
+  var ip = json.wifi.sta_ip || json.wifi.ap_ip;
+  let html = json.arch + '/' + json.fw_id + ', IP: ';
+  if (ip) {
+    html += '<a target="_blank" href=http://' + ip + '>' + ip + '</a>';
+  } else {
+    html += 'n/a';
+  }
+  $('#devinfo').html(html);
+});
+
+$('#app_view').resizable({
+  handleSelector: ".splitter-horizontal",
+  resizeWidth: false
+});
+
+$('#d1').height($(document.body).height() - 60);
+$('#app_view').height($(d1).height() * 0.75);
+$('#device-logs-panel').height($(d1).height() * 0.25);
+
+var guesslang = function(filename) {
+  if ((filename || '').match(/\.js$/)) return 'javascript';
+  if ((filename || '').match(/\.json$/)) return 'json';
+  if ((filename || '').match(/\.ya?ml$/)) return 'yaml';
+  return 'text';
+};
+var mkeditor = function(id, lang) {
+  var editor = ace.edit(id || 'editor');
+  editor.$blockScrolling = Infinity;
+  editor.setTheme('ace/theme/tomorrow');
+  editor.setOptions({
+    highlightActiveLine: false,
   });
-
-  $(document)
-      .on('click', '#reboot-button', function() {
-        $.ajax({url: '/call', data: {method: 'Sys.Reboot'}}).done(function() {
-          new PNotify({title: 'Device rebooted', type: 'success'});
-        });
-      });
-
-  $(document).on('click', '#clear-logs-button', function() {
-    $('#device-logs').empty();
-    new PNotify({title: 'Console cleared', type: 'success'});
+  editor.renderer.setOptions({
+    fontSize: '14px',
+    fontFamily: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
   });
-
-  var connected = false;
-
-  // A page which needs to be loaded once serial port connection is established
-  var deferredLoadPage = undefined;
-
-  var loadPage = function(page) {
-    var doit = function(html) {
-      $('#app_view').html(html);
-      $('#breadcrumb').html($('[data-title]').attr('data-title'));
-    };
-    if (pageCache[page]) {
-      doit(pageCache[page]);
-    } else {
-      $.get('page_' + page + '.html').done(function(html) {
-        pageCache[page] = html;
-        doit(html);
-      });
-    }
-  };
-
-  $(document).on('click', 'a[tab]', function() {
-    var page = $(this).attr('tab');
-    loadPage(page);
+  editor.session.setOptions({
+    tabSize: 2,
+    useSoftTabs: true,
   });
-
-  $(document).ready(function() {
-    $('a[tab]').first().click();
-  });
-
-  $.ajax({url: '/call', data: {method: 'Sys.GetInfo'}}).then(function(data) {
-    var json = data.result;
-    var ip = json.wifi.sta_ip || json.wifi.ap_ip;
-    let html = '<b>' + json.arch + '/' + json.fw_id + '</b>, IP: ';
-    if (ip) {
-      html += '<a target="_blank" href=http://' + ip + '>' + ip + '</a>';
-    } else {
-      html += 'n/a';
-    }
-    $('#devinfo').html(html);
-  });
-
-  $('#app_view').resizable({
-    handleSelector: ".splitter-horizontal",
-    resizeWidth: false
-  });
-
-  $('#d1').height($(document.body).height() - 60);
-  $('#app_view').height($(d1).height() * 0.75);
-  $('#device-logs-panel').height($(d1).height() * 0.25);
-
-})(jQuery);
+  if (lang) editor.session.setMode('ace/mode/' + lang);
+  return editor;
+};
