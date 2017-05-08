89f0d44cbcfcd5e384c56bf830cfa015e5a2dae0
Ruslan Valiullin <r.valiullin77@gmail.com>
2017-05-08 18:49:24 +0300

    Add Arduino dallas temp library example
    PUBLISHED_FROM=2c29b30b197bbb047cde278541a6c88da1d4e1c1

--- fw/examples/c_arduino_dallas_temp/src/main.cpp
+++ fw/examples/c_arduino_dallas_temp/src/main.cpp
@@ -6,19 +6,42 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-OneWire *ow;
-DallasTemperature *dt;
+#define ONEWIRE_PIN 13 // 1-Wire bus is plugged into GPIO13
+#define NUM_SENS 2 // Number of sensors on the 1-Wire bus
+
+OneWire *ow = NULL;
+DallasTemperature *dt = NULL;
+DeviceAddress sens[NUM_SENS];
 
 void setup(void) {
-  printf("Arduino Dallas Temperature sensors library example\n");
+  printf("Arduino Dallas Temperature library very simple example\n");
 
-  // 1-Wire bus is plugged into GPIO13
-  ow = new OneWire(13);
+  // Initialize a OneWire handle
+  ow = new OneWire(ONEWIRE_PIN);
+  // Pass a OneWire handle to Dallas Temperature
   dt = new DallasTemperature(ow);
+  // Start up the library
   dt->begin();
+
+  // Search for devices on the 1-Wire bus
+  ow->reset_search();
+  for (int i = 0; i < NUM_SENS; i++) {
+    if (!ow->search(sens[i])) {
+      printf("Can't find sensor #%d\n", i+1);
+    } else {
+      printf("Sens#%d address: ", i+1);
+      for (int j = 0; j < 8; j++) {
+        printf("%x", sens[i][j]);
+      }
+      printf("\n");
+    }
+  }
 }
 
 void loop(void) {
   dt->requestTemperatures();
-  printf("Temperature: %f *C\n", dt->getTempCByIndex(0));
+  delay(1000);
+  for (int i = 0; i < NUM_SENS; i++) {
+    printf("Sens#%d Temperature: %f *C\n", i+1, dt->getTempC(sens[i]));
+  }
 }
