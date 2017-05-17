518aa8916a0d9ed893b0faeb4de6dae5e7fb6a40
Deomid Ryabkov <rojer@cesanta.com>
2017-05-17 14:09:34 +0500

    Amend SSD1306 example
    Make resolution runtime-selectable and support multiple displays.
    
    Driver change is backward-compatible and has been submitted upstream as https://github.com/adafruit/Adafruit_SSD1306/pull/83
    
    PUBLISHED_FROM=7c958fc3d58c680f38f49332be3e9fcd04214756

--- fw/examples/c_arduino_ssd1306/src/main.cpp
+++ fw/examples/c_arduino_ssd1306/src/main.cpp
@@ -10,37 +10,51 @@
 #include "fw/src/mgos_app.h"
 #include "fw/src/mgos_timers.h"
 
-Adafruit_SSD1306 *display = nullptr;
+Adafruit_SSD1306 *d1 = nullptr, *d2 = nullptr;
 
 static void timer_cb(void *arg);
 
 void setup(void) {
   // I2C
-  display = new Adafruit_SSD1306(5 /* RST GPIO */);
+  d1 = new Adafruit_SSD1306(4 /* RST GPIO */, Adafruit_SSD1306::RES_128_64);
+
+  if (d1 != nullptr) {
+    d1->begin(SSD1306_SWITCHCAPVCC, 0x3D, true /* reset */);
+    d1->display();
+  }
 
   // SPI.
   // On ESP8266 SPI and I2C cannot be enabled at the same time by default
   // because their pins overlap.
   // You will need to disable I2C and enable SPI:
   // mos config-set i2c.enable=false spi.enable=true
-  // display = new Adafruit_SSD1306(21 /* DC */, 5 /* RST */, 17 /* CS */);
+  // d2 = new Adafruit_SSD1306(21 /* DC */, 5 /* RST */, 17 /* CS */,
+  //                          Adafruit_SSD1306::RES_128_32);
 
-  display->begin(SSD1306_SWITCHCAPVCC);
-  display->display();  // Displays the Adafruit logo.
+  if (d2 != nullptr) {
+    d2->begin(SSD1306_SWITCHCAPVCC, 0 /* unused */, true /* reset */);
+    d2->display();
+  }
 
   mgos_set_timer(1000 /* ms */, true /* repeat */, timer_cb, NULL);
 }
 
+static void show_num(Adafruit_SSD1306 *d, const char *s, int i) {
+  d->clearDisplay();
+  d->setTextSize(2);
+  d->setTextColor(WHITE);
+  d->setCursor(d->width() / 4, d->height() / 4);
+  d->printf("%s%d", s, i);
+  d->display();
+}
+
 static void timer_cb(void *arg) {
-  static int i = 0;
-  display->clearDisplay();
-  display->setTextSize(2);
-  display->setTextColor(WHITE);
-  display->setCursor(32, 8);
-  display->printf("i = %d", i);
-  display->display();
-  LOG(LL_INFO, ("i = %d", i));
+  static int i = 0, j = 0;
+  if (d1 != nullptr) show_num(d1, "i = ", i);
+  if (d2 != nullptr) show_num(d2, "j = ", j);
+  LOG(LL_INFO, ("i = %d, j = %d", i, j));
   i++;
+  j++;
   (void) arg;
 }
 
