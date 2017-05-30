b3f8046c399c4cba958750ff1c19163ed57f9975
Deomid Ryabkov <rojer@cesanta.com>
2017-05-30 19:33:51 +0100

    Change default topic to be compatible with GCP
    PUBLISHED_FROM=550cab70dd8f9cfc36c189fc2abdcd1a8fb753a0

--- fw/examples/mjs_base/fs/init.js
+++ fw/examples/mjs_base/fs/init.js
@@ -20,7 +20,7 @@ Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
 
 // Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
 GPIO.set_button_handler(0, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
-  let topic = 'devices/' + Cfg.get('device.id');
+  let topic = '/devices/' + Cfg.get('device.id') + '/events';
   let message = getInfo();
   let ok = MQTT.pub(topic, message, 1);
   print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
