55c0e928051275aee4f40d5e1b6afb890f6807fd
Dmitry Frank <mail@dmitryfrank.com>
2017-06-03 03:23:45 +0300

    Use mjs lib in mjs_base example
    PUBLISHED_FROM=b822b342c8211047319d155f07bd377791434fa3

--- fw/examples/mjs_base/src/main.c
+++ fw/examples/mjs_base/src/main.c
@@ -31,19 +31,5 @@ int get_led_gpio_pin(void) {
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-  /* Initialize JavaScript engine */
-  int mem1, mem2, mem3;
-  mem1 = mgos_get_free_heap_size();
-  struct mjs *mjs = mjs_create();
-  mem2 = mgos_get_free_heap_size();
-  mjs_set_ffi_resolver(mjs, mgos_dlsym);
-  mjs_err_t err = mjs_exec_file(mjs, "init.js", 1, NULL);
-  if (err != MJS_OK) {
-    mjs_print_error(mjs, stdout, NULL, 1 /* print_stack_trace */);
-  }
-  mem3 = mgos_get_free_heap_size();
-  LOG(LL_INFO, ("mJS memory stat: before init: %d "
-                "after init: %d after init.js: %d",
-                mem1, mem2, mem3));
   return MGOS_APP_INIT_SUCCESS;
 }
