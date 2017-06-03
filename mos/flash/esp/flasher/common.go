2f549e091c70a65311f7b5f034fa00f457c94676
Dmitry Frank <github@dmitryfrank.com>
2017-06-03 03:25:17 +0300

    More friendly message when cannot connect to ROM
    PUBLISHED_FROM=88ac8a7b6e4b9abdbc1290c6af98d8dd8229145b

--- mos/flash/esp/flasher/common.go
+++ mos/flash/esp/flasher/common.go
@@ -45,7 +45,13 @@ func ConnectToFlasherClient(ct esp.ChipType, opts *esp.FlashOpts) (*cfResult, er
 
 	r.rc, err = rom_client.ConnectToROM(ct, opts)
 	if err != nil {
-		return nil, errors.Trace(err)
+		return nil, errors.Annotatef(
+			err,
+			"Failed to talk to bootloader. See "+
+				"https://mongoose-os.com/docs/#/quickstart/flash.md/ "+
+				"for wiring instructions or put the device into flashing mode manually by "+
+				"pulling GPIO0 low and resetting.",
+		)
 	}
 	ownROMClient := true
 	defer func() {
