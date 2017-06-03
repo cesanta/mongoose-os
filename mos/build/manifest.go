4f3cec16eaa57b16f4d8fdeec5b433e595113757
Dmitry Frank <mail@dmitryfrank.com>
2017-06-03 12:44:05 +0300

    Expand @all_libs in the manifest
    PUBLISHED_FROM=386374d99dc3d4a2bc20c23eb502021e6c9057a4

--- mos/build/manifest.go
+++ mos/build/manifest.go
@@ -15,6 +15,11 @@ type AppManifest struct {
 	Summary string `yaml:"summary,omitempty" json:"summary,omitempty"`
 }
 
+type FWAppManifestLibHandled struct {
+	Name string `yaml:"name" json:"name,omitempty"`
+	Path string `yaml:"path" json:"path,omitempty"`
+}
+
 // FWAppManifest is the app manifest for firmware apps
 type FWAppManifest struct {
 	AppManifest       `yaml:",inline"`
@@ -37,10 +42,14 @@ type FWAppManifest struct {
 	CDefs             map[string]string  `yaml:"cdefs" json:"cdefs,omitempty"`
 	Tags              []string           `yaml:"tags" json:"tags,omitempty"`
 
-	// Deps are names of the libraries which need to be initialized before the
+	// Old form of LibsHandled, deprecated since 03.06.2017
+	// TODO: remove
+	Deps []string `yaml:"deps" json:"deps,omitempty"`
+
+	// are names of the libraries which need to be initialized before the
 	// application. The user doesn't have to set this field manually, it's set
 	// automatically during libs "expansion" (see Libs above)
-	Deps []string `yaml:"deps" json:"deps,omitempty"`
+	LibsHandled []FWAppManifestLibHandled `yaml:"libs_handled" json:"libs_handled,omitempty"`
 }
 
 // ConfigSchemaItem represents a single config schema item, like this:
