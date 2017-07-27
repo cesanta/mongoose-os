3018606c9ceda516ed3ab358374d866bcd2ecc0f
Deomid Ryabkov <rojer@cesanta.com>
2017-07-27 12:35:49 +0200

    Add upload-deb.sh, for convenience
    PUBLISHED_FROM=3d04d505f1fe62a84c1411af8d29db387ac8b764

---
+++ mos/ubuntu/upload-deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PACKAGE=$1
+DISTR=$2
+
+[ -z "${PACKAGE}" -o -z "${DISTR}" ] && { echo "Usage: $0 package distr"; exit 1; }
+
+[ -z "${GNUPGHOME}" ] && GNUPGHOME=$HOME/.gnupg-cesantabot
+[ -d "${GNUPGHOME}" ] || { echo "${GNUPGHOME} does not exist"; exit 1; }
+
+OUTDIR="/tmp/out-${DISTR}"
+
+set -x -e
+
+# Note: always using Xenial because Zesty has problem with passphrase entry.
+docker run -it --rm \
+    -v ${GNUPGHOME}:/root/.gnupg \
+    -v ${OUTDIR}:/work \
+    docker.cesanta.com/ubuntu-golang:xenial \
+    /bin/bash -l -c "\
+        cd /work && \
+        debsign *_source.changes && \
+        dput ppa:mongoose-os/mos *_source.changes"
