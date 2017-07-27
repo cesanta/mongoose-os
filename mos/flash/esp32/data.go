d9a60bf0dbd40de8e8ff347c0e3a131372ca9233
Deomid Ryabkov <rojer@cesanta.com>
2017-07-27 23:37:53 +0300

    Roll back CI go build images to 1.6; add 32-bit
    Remove Bazel from OurCI image, it no longer used and the image no longer builds
    
    PUBLISHED_FROM=c2ac2e4be17c45c02b5d5e99fe9b3fc6fc3d5c6e

--- mos/flash/esp32/data.go
+++ mos/flash/esp32/data.go
@@ -4,4 +4,4 @@ package esp32
 //  go generate cesanta.com/mos/flash/esp32 && go build -v && ./mos flash ...
 //
 // DISABLED go:generate ./genstubs.sh
-//go:generate go-bindata -pkg esp32 -modtime 1 -mode 420 data/
+//go:generate go-bindata -pkg esp32 -nocompress -modtime 1 -mode 420 data/
--- mos/flash/esp8266/data.go
+++ mos/flash/esp8266/data.go
@@ -4,4 +4,4 @@ package esp8266
 //  go generate cesanta.com/mos/flash/esp && go build -v && ./mos flash ...
 //
 // DISABLED go:generate ./genstubs.sh
-//go:generate go-bindata -pkg esp8266 -modtime 1 -mode 420 data/
+//go:generate go-bindata -pkg esp8266 -nocompress -modtime 1 -mode 420 data/
