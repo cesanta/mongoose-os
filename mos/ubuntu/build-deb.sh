7448473481243d0cc78baab1e340b224b9f95214
Dmitry Frank <mail@dmitryfrank.com>
2017-08-08 11:15:56 +0100

    Pull docker image before building deb
    PUBLISHED_FROM=cf2788ff9291a2e0832bae680ce7310fbc181048

--- mos/ubuntu/build-deb.sh
+++ mos/ubuntu/build-deb.sh
@@ -7,10 +7,13 @@ DISTR=$2
 
 set -x -e
 
+IMAGE=docker.cesanta.com/ubuntu-golang:${DISTR}
+
+docker pull ${IMAGE}
 docker run -i -t --rm \
     -v $PWD:/src \
     -v /tmp/out-${DISTR}:/tmp/work \
-    docker.cesanta.com/ubuntu-golang:${DISTR} \
+    ${IMAGE} \
     /bin/bash -l -c "\
         cd /src && rm -rf /tmp/work/* && \
         git-build-recipe --allow-fallback-to-native --package ${PACKAGE} --distribution ${DISTR} \
