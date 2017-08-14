bedee69bfe979fb3c54b295d55b13950b684734f
Dmitry Frank <mail@dmitryfrank.com>
2017-08-14 15:00:20 +0100

    Add tools/deploy_mos.py
    So far this script doesn't work on mac for some reason, so deploying mos
    binaries for mac ans win is still a separate step from mac. Other than
    that:
    
    Deploy latest mos debs for xenial and zesty:
    
    ```
    $ python tools/deploy_mos.py
    ```
    
    Deploy released firmware and released debs for xenial and zesty:
    
    ```
    $ python tools/deploy_mos.py --release-tag 1.14
    ```
    
    PUBLISHED_FROM=5af91e0a650b25a99d39e88f6ad6bd5f159a8b76

--- mos/ubuntu/build-deb.sh
+++ mos/ubuntu/build-deb.sh
@@ -2,8 +2,27 @@
 
 PACKAGE=$1
 DISTR=$2
+VERSION=$3
 
-[ -z "${PACKAGE}" -o -z "${DISTR}" ] && { echo "Usage: $0 package distr"; exit 1; }
+[ -z "${PACKAGE}" -o -z "${DISTR}" ] && { echo "Usage: $0 <package> <distr> [<version>]"; exit 1; }
+
+RECIPE_ORIG_FILE=$PWD/mos/ubuntu/${PACKAGE}-${DISTR}.recipe
+
+if [ -z "${VERSION}" ]; then
+  # Version was not given; use the recipe as it is
+  RECIPE=/src/mos/ubuntu/${PACKAGE}-${DISTR}.recipe
+  RECIPE_MOUNT=
+else
+  # Version is given: replace 1.11_CHANGE_ME with the actual version
+  RECIPE_TMP_DIR=/tmp/recipe-${PACKAGE}-${DISTR}-${VERSION}
+  mkdir -p ${RECIPE_TMP_DIR}
+  RECIPE_HOST_FILE=${RECIPE_TMP_DIR}/recipe
+
+  cat ${RECIPE_ORIG_FILE} | sed "s/1\.11_CHANGE_ME/${VERSION}/" > ${RECIPE_HOST_FILE}
+
+  RECIPE=/recipe/recipe
+  RECIPE_MOUNT=" -v ${RECIPE_TMP_DIR}:/recipe"
+fi
 
 set -x -e
 
@@ -19,11 +38,12 @@ docker pull ${IMAGE}
 docker run -i -t --rm \
     -v $PWD:/src \
     -v /tmp/out-${DISTR}:/tmp/work \
+    ${RECIPE_MOUNT} \
     ${IMAGE} \
     /bin/bash -l -c "\
         cd /src && rm -rf /tmp/work/* && \
         git-build-recipe --allow-fallback-to-native --package ${PACKAGE} --distribution ${DISTR} \
-            /src/mos/ubuntu/${PACKAGE}-${DISTR}.recipe /tmp/work && \
+            ${RECIPE} /tmp/work && \
         cd /tmp/work/${PACKAGE} && \
         debuild --no-tgz-check -us -uc -b"
 
