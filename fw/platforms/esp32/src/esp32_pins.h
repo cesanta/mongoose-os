c42d36355e575d386d637d08a2153769bd619185
Deomid Ryabkov <rojer@cesanta.com>
2017-10-24 20:02:24 +0100

    Add mapping of WROOM32 pins to GPIOs
    For convenience, so, e.g:
    
    ```
     #include "esp32_pins.h"
    
     #define FOO_GPIO WROOM32_PIN_10_GPIO
    ```
    
    PUBLISHED_FROM=13cf7f28eedc358b4a817c495096b8973614aeff

---
+++ fw/platforms/esp32/src/esp32_pins.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_FW_PLATFORMS_ESP32_SRC_ESP32_PINS_H_
+#define CS_FW_PLATFORMS_ESP32_SRC_ESP32_PINS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Pin to GPIO mappings for ESP-WROOM-32 */
+
+/* Pin 1: GND */
+/* Pin 2: 3V3 */
+/* Pin 3: EN */
+#define WROOM32_PIN_4_GPIO 36
+#define WROOM32_PIN_5_GPIO 39
+#define WROOM32_PIN_6_GPIO 34
+#define WROOM32_PIN_7_GPIO 35
+#define WROOM32_PIN_8_GPIO 32
+#define WROOM32_PIN_9_GPIO 33
+#define WROOM32_PIN_10_GPIO 25
+#define WROOM32_PIN_11_GPIO 26
+#define WROOM32_PIN_12_GPIO 27
+#define WROOM32_PIN_13_GPIO 14
+#define WROOM32_PIN_14_GPIO 12
+/* Pin 15: GND */
+#define WROOM32_PIN_16_GPIO 13
+/* Pins 17-22: SFLASH pins */
+#define WROOM32_PIN_23_GPIO 15
+#define WROOM32_PIN_24_GPIO 2
+#define WROOM32_PIN_25_GPIO 0
+#define WROOM32_PIN_26_GPIO 4
+#define WROOM32_PIN_27_GPIO 16
+#define WROOM32_PIN_28_GPIO 17
+#define WROOM32_PIN_29_GPIO 5
+#define WROOM32_PIN_30_GPIO 18
+#define WROOM32_PIN_31_GPIO 19
+/* Pin 32: NC */
+#define WROOM32_PIN_33_GPIO 21
+#define WROOM32_PIN_34_GPIO 3
+#define WROOM32_PIN_35_GPIO 1
+#define WROOM32_PIN_36_GPIO 22
+#define WROOM32_PIN_37_GPIO 23
+/* Pin 38: GND */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CS_FW_PLATFORMS_ESP32_SRC_ESP32_PINS_H_ */
