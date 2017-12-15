2b9d3575a639ce4e43c72e85e6faf950c5a1132f
Dmitry Frank <mail@dmitryfrank.com>
2017-12-15 23:28:02 +0200

    Improve docs in mgos_mongoose.h
    Also move hal to internal header
    
    PUBLISHED_FROM=a2c60a035abe0b692906258bd3da3af88cf446c2

--- fw/include/mgos_mongoose.h
+++ fw/include/mgos_mongoose.h
@@ -47,53 +47,31 @@ void mongoose_destroy(void);
 typedef void (*mgos_poll_cb_t)(void *cb_arg);
 
 /*
- * Add an on-poll callback with an arbitrary argument.
+ * Add an on-poll callback with an arbitrary argument, see `mongoose_poll()`.
  */
 void mgos_add_poll_cb(mgos_poll_cb_t cb, void *cb_arg);
 
 /*
- * Remove an on-poll callback.
+ * Remove an on-poll callback, see `mongoose_poll()`.
  */
 void mgos_remove_poll_cb(mgos_poll_cb_t cb, void *cb_arg);
 
 /*
- * Set whether wdt should be fed on each `mongoose_poll`.
+ * Set whether wdt should be fed on each call to `mongoose_poll()`.
  */
 void mgos_wdt_set_feed_on_poll(bool enable);
 
 /*
- * Set whether min free heap size should be reported on each `mongoose_poll`.
+ * Set whether min free heap size should be reported on each call to
+ * `mongoose_poll()`.
  */
 void mgos_set_enable_min_heap_free_reporting(bool enable);
 
 /*
- * Returns nameserver address. The caller should `free()` it.
+ * Returns nameserver address. The caller should `free()` it. Returns NULL
+ * in case of no DNS.
  */
 char *mgos_get_nameserver(void);
-
-/* HAL */
-
-/* Schedule MG poll ASAP. Note: may be called from ISR context. */
-void mongoose_schedule_poll(bool from_isr);
-
-struct mg_connection *mgos_bind(const char *addr, mg_event_handler_t func,
-                                void *ud);
-struct mg_connection *mgos_connect(const char *addr, mg_event_handler_t func,
-                                   void *ud);
-struct mg_connection *mgos_connect_ssl(const char *addr, mg_event_handler_t f,
-                                       void *ud, const char *cert,
-                                       const char *key, const char *ca_cert);
-void mgos_disconnect(struct mg_connection *c);
-
-struct mg_connection *mgos_bind_http(const char *addr);
-bool mgos_add_http_endpoint(struct mg_connection *c, const char *uri,
-                            mg_event_handler_t handler, void *user_data);
-struct mg_connection *mgos_connect_http(const char *addr, mg_event_handler_t,
-                                        void *ud);
-struct mg_connection *mgos_connect_http_ssl(const char *addr,
-                                            mg_event_handler_t f, void *ud,
-                                            const char *cert, const char *key,
-                                            const char *ca_cert);
 
 #ifdef __cplusplus
 }
--- fw/src/mgos_mongoose_internal.h
+++ fw/src/mgos_mongoose_internal.h
@@ -21,6 +21,30 @@ extern "C" {
  */
 enum mgos_init_result mongoose_init(void);
 
+/* HAL */
+
+/* Schedule MG poll ASAP. Note: may be called from ISR context. */
+void mongoose_schedule_poll(bool from_isr);
+
+struct mg_connection *mgos_bind(const char *addr, mg_event_handler_t func,
+                                void *ud);
+struct mg_connection *mgos_connect(const char *addr, mg_event_handler_t func,
+                                   void *ud);
+struct mg_connection *mgos_connect_ssl(const char *addr, mg_event_handler_t f,
+                                       void *ud, const char *cert,
+                                       const char *key, const char *ca_cert);
+void mgos_disconnect(struct mg_connection *c);
+
+struct mg_connection *mgos_bind_http(const char *addr);
+bool mgos_add_http_endpoint(struct mg_connection *c, const char *uri,
+                            mg_event_handler_t handler, void *user_data);
+struct mg_connection *mgos_connect_http(const char *addr, mg_event_handler_t,
+                                        void *ud);
+struct mg_connection *mgos_connect_http_ssl(const char *addr,
+                                            mg_event_handler_t f, void *ud,
+                                            const char *cert, const char *key,
+                                            const char *ca_cert);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
