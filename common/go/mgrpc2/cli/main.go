ea7f0efcb1da50fcf24b50ed62400e454306cfc8
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-12-27 13:05:48 +0200

    dash: further fixes in device panels, mocking device with mgrpc
    dash: fix websocket reconnects
    
    PUBLISHED_FROM=d2147edc719faf66abc1715c8f43bfeca3dfd868

--- common/go/mgrpc2/cli/main.go
+++ common/go/mgrpc2/cli/main.go
@@ -1,35 +1,118 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
 	"golang.org/x/net/context"
 
 	"cesanta.com/common/go/mgrpc2"
 )
 
+func handleStdin(framec chan mgrpc2.Frame) {
+	r := bufio.NewReader(os.Stdin)
+	for {
+		if text, err := r.ReadString('\n'); err != nil {
+			break
+		} else if parts := strings.SplitN(text, " ", 2); len(parts) < 1 || len(parts) > 2 || parts[0] == "\n" {
+			log.Println("Invalid input. Expected: 'method args'")
+		} else {
+			frame := mgrpc2.Frame{Method: parts[0]}
+			if len(parts) > 1 {
+				frame.Args = json.RawMessage(parts[1])
+			}
+			framec <- frame
+		}
+	}
+}
+
+func handleConfig(name string, d mgrpc2.Dispatcher, framec chan mgrpc2.Frame) {
+	config := struct {
+		Config string `json:"config"`
+	}{}
+	b, err := ioutil.ReadFile(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	config.Config = string(b)
+
+	d.AddHandler("Config.Get", func(d mgrpc2.Dispatcher, c mgrpc2.Channel, f *mgrpc2.Frame) *mgrpc2.Frame {
+		return &mgrpc2.Frame{Result: json.RawMessage(config.Config)}
+	})
+	d.AddHandler("Config.Set", func(d mgrpc2.Dispatcher, c mgrpc2.Channel, f *mgrpc2.Frame) *mgrpc2.Frame {
+		if err := json.Unmarshal([]byte(f.Args), &config); err != nil {
+			return &mgrpc2.Frame{Result: json.RawMessage(`false`)}
+		} else if err := ioutil.WriteFile(name, []byte(f.Args), 0644); err != nil {
+			return &mgrpc2.Frame{Result: json.RawMessage(`false`)}
+		}
+		config.Config = string([]byte(f.Args))
+		return &mgrpc2.Frame{Result: json.RawMessage(`true`)}
+	})
+}
+
+func handleShadow(name string, d mgrpc2.Dispatcher, framec chan mgrpc2.Frame) {
+	shadow := "{}"
+	d.AddHandler("Dash.Shadow.Get", func(d mgrpc2.Dispatcher, c mgrpc2.Channel, f *mgrpc2.Frame) *mgrpc2.Frame {
+		return &mgrpc2.Frame{Result: json.RawMessage(shadow)}
+	})
+	d.AddHandler("Dash.Shadow.Update", func(d mgrpc2.Dispatcher, c mgrpc2.Channel, f *mgrpc2.Frame) *mgrpc2.Frame {
+		shadow = string([]byte(f.Args))
+		return &mgrpc2.Frame{Result: json.RawMessage(`true`)}
+	})
+}
+
 func main() {
 	dst := flag.String("dst", "", "RPC destination")
-	method := flag.String("method", "", "Method name to call")
 	key := flag.String("key", "", "RPC frame key (password)")
-	args := flag.String("args", "", "RPC command arguments")
+	config := flag.String("config", "", "Device config JSON")
+	shadow := flag.String("shadow", "", "Device shadow JSON")
+
 	flag.Parse()
 
-	if *dst == "" || *method == "" {
-		log.Fatalf("Please specify --dst and --method")
+	if *dst == "" {
+		log.Fatalf("Please specify --dst")
 	}
+
 	d := mgrpc2.CreateDispatcher()
-	req := mgrpc2.Frame{Dst: *dst, Method: *method, Key: *key}
-	if *args != "" {
-		req.Args = json.RawMessage(*args)
+	d.AddHandler("*", func(d mgrpc2.Dispatcher, c mgrpc2.Channel, f *mgrpc2.Frame) *mgrpc2.Frame {
+		switch f.Method {
+		case "RPC.List":
+			// TODO
+		case "RPC.Describe":
+			// TODO
+		default:
+			log.Println(f)
+		}
+		return &mgrpc2.Frame{Result: json.RawMessage(`true`)}
+	})
+
+	framec := make(chan mgrpc2.Frame, 1)
+	framec <- mgrpc2.Frame{Method: "Dash.Heartbeat"}
+	go handleStdin(framec)
+
+	if *config != "" {
+		go handleConfig(*config, d, framec)
 	}
-	res, err := d.Call(context.Background(), &req)
-	if err != nil {
-		log.Fatalf("Error calling: %s@%s: %v", *method, *dst, err)
+
+	if *shadow != "" {
+		go handleShadow(*shadow, d, framec)
 	}
-	s, _ := json.Marshal(&res)
-	fmt.Println(string(s))
+
+	for frame := range framec {
+		frame.Dst = *dst
+		frame.Key = *key
+		res, err := d.Call(context.Background(), &frame)
+		if err != nil {
+			log.Fatalf("Error calling: %v: %v", frame, err)
+		} else {
+			s, _ := json.Marshal(&res)
+			fmt.Println(string(s))
+		}
+	}
 }
