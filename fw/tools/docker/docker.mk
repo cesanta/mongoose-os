9a449278552f5252ebb51725291666eba2670395
Deomid Ryabkov <rojer@cesanta.com>
2018-05-23 11:45:01 +0100

    Add missing file
    PUBLISHED_FROM=7756c7a30d46ce26dae2ca38885036231457776e

---
+++ fw/tools/docker/docker.mk
@@ -0,0 +1,41 @@
+# Makefiles including this should set the DOCKERFILES var
+# to a list of image names.
+#
+# For each image $(i) the following targets will be defined:
+#
+# docker-build-$(i): builds the image from Dockerfile-$(i)
+# docker-push-$(i): pushes the image as $(REGISTRY)/$(i)
+
+REPO_PATH ?= $(realpath ../../../..)
+REGISTRY ?= docker.cesanta.com
+DOCKER_TAG ?= latest
+DOCKER_FLAGS ?=
+
+all: docker-build
+
+docker-build: $(foreach i,$(DOCKERFILES),docker-build-$(i))
+docker-push: $(foreach i,$(DOCKERFILES),docker-push-$(i))
+
+docker-pre-build-%: Dockerfile-%
+	@true
+
+docker-build-%: docker-pre-build-%
+	@echo Building $(REGISTRY)/$*:$(DOCKER_TAG)
+	docker build $(DOCKER_FLAGS) -t $(REGISTRY)/$*:$(DOCKER_TAG) -f Dockerfile-$* .
+	@echo Built $(REGISTRY)/$*:$(DOCKER_TAG)
+
+docker-push-%:
+	docker push $(REGISTRY)/$*:$(DOCKER_TAG)
+
+fw_meta.py: $(REPO_PATH)/common/tools/fw_meta.py
+	cp -v $< .
+
+serve_core.py: $(REPO_PATH)/common/tools/serve_core.py
+	cp -v $< .
+
+mkspiffs mkspiffs8: $(wildcard $(REPO_PATH)/common/spiffs/*)
+	docker run --rm -it -v $(REPO_PATH):/cesanta \
+	  docker.cesanta.com/gcc \
+	  bash -c 'make -C /cesanta/common/spiffs/tools mkspiffs mkspiffs8 \
+	    SPIFFS_CONFIG_PATH=$(SPIFFS_CONFIG_PATH)'
+	cp -v $(REPO_PATH)/common/spiffs/tools/$@ $@
