5589efb8efa0bdcb221e56032e34c050396cd7d2
Deomid Ryabkov <rojer@cesanta.com>
2018-06-28 15:49:25 +0300

    Fix macro redefinition
    CL: none
    
    PUBLISHED_FROM=87ae304301ebb273d5f9bef721270a2b87d7f6eb

--- fw/platforms/stm32/include/stm32f4xx_hal_conf.h
+++ fw/platforms/stm32/include/stm32f4xx_hal_conf.h
@@ -344,24 +344,6 @@ extern "C" {
 #include "stm32f4xx_hal_spdifrx.h"
 #endif /* HAL_SPDIFRX_MODULE_ENABLED */
 
-/* Exported macro ------------------------------------------------------------*/
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  The assert_param macro is used for function's parameters check.
-  * @param  expr: If expr is false, it calls assert_failed function
-  *         which reports the name of the source file and the source
-  *         line number of the call that failed.
-  *         If expr is true, it returns no value.
-  * @retval None
-  */
-#define assert_param(expr) \
-  ((expr) ? (void) 0 : assert_failed((uint8_t *) __FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-void assert_failed(uint8_t *file, uint32_t line);
-#else
-#define assert_param(expr) ((void) 0U)
-#endif /* USE_FULL_ASSERT */
-
 #ifdef __cplusplus
 }
 #endif
