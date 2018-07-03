8fdee22a56d44c72c44ffd445b52b0a3160839d1
Deomid Ryabkov <rojer@cesanta.com>
2018-07-03 17:23:22 +0300

    Stringify multi-word macros correctly
    Use vararg macros for this.
    
    PUBLISHED_FROM=735c40b79dedef17a1fdbb5b846e984df0ae7018

--- common/str_util.h
+++ common/str_util.h
@@ -36,7 +36,11 @@
  * Expands to a string representation of its argument: e.g.
  * `CS_STRINGIFY_LIT(5) expands to "5"`
  */
+#if !defined(_MSC_VER) || _MSC_VER >= 1900
+#define CS_STRINGIFY_LIT(...) #__VA_ARGS__
+#else
 #define CS_STRINGIFY_LIT(x) #x
+#endif
 
 /*
  * Expands to a string representation of its argument, which is allowed
