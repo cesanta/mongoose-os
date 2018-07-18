3562752c137722565161a0b013a352abd8efc795
Deomid Ryabkov <rojer@cesanta.com>
2018-07-18 14:49:06 +0300

    Minor fixes and refactorings
    To facilitate STM32 boot loader work.
    
    CL: none
    
    PUBLISHED_FROM=37b453444373a99e24e9182d108cd955d0850051

--- fw/platforms/stm32/include/stm32_sdk_hal.h
+++ fw/platforms/stm32/include/stm32_sdk_hal.h
@@ -20,8 +20,10 @@
 
 #if defined(STM32F4)
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_ll_utils.h"
 #elif defined(STM32F7)
 #include "stm32f7xx_hal.h"
+#include "stm32f7xx_ll_utils.h"
 #else
 #error Device family not defined
 #endif
