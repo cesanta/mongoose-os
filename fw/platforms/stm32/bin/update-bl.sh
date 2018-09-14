0cc45a96b532994bab18182a95333c26cad4911d
Deomid Ryabkov <rojer@cesanta.com>
2018-09-14 23:07:03 +0300

    STM32: Update boot loader binaries
    PUBLISHED_FROM=1cff5081523bb65c275b483e84b591c841b4a10a

---
+++ fw/platforms/stm32/bin/update-bl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -x
+
+for b in B-L475E-IOT01A DISCO-F746NG; do
+  rm -f *.zip
+  curl -qsSLO https://github.com/mongoose-os-apps/bootloader/releases/download/latest/bootloader-stm32-$b.zip
+  unzip -p bootloader-*.zip bootloader-*/bootloader.bin > bootloader-$b.bin
+  rm -f *.zip
+done
