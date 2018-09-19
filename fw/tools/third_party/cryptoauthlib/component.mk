1d0a7e01dae30fd93ad1592d723108b3a003838a
Deomid Ryabkov <rojer@cesanta.com>
2018-09-19 17:38:47 +0300

    Move CryptoAuthLib sources to the ATCA library
    Only the necessary bits
    
    CL: none
    
    PUBLISHED_FROM=5bd09ca4a446c92428a03d269cc3edd50c913b08

--- fw/tools/third_party/cryptoauthlib/component.mk
+++
@@ -1,8 +0,0 @@
-#
-# ESP32 SDK build system component Makefile
-#
-
-COMPONENT_ADD_INCLUDEDIRS := port/include include
-
-COMPONENT_SRCDIRS := lib lib/basic lib/host
-COMPONENT_ADD_INCLUDEDIRS := lib lib/hal
