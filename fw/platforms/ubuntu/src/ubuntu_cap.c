8301b1045218c02f286d032fdc9b71503f81f0f7
Pim van Pelt <pim@ipng.nl>
2019-01-23 18:10:01 +0100

    Accommodate users running as root but not setting -g/-u explicitly, and take the permissions of the conf0.json file

--- fw/platforms/ubuntu/src/ubuntu_cap.c
+++ fw/platforms/ubuntu/src/ubuntu_cap.c
@@ -24,13 +24,14 @@ static bool ubuntu_cap_have(cap_value_t c) {
 }
 
 bool ubuntu_cap_init(void) {
+  struct stat s;
+  char        conf_fn[200];
+
   if (!ubuntu_cap_have(CAP_NET_BIND_SERVICE)) {
-    printf("WARNING: Lacking capability to bind ports <1024, continuing anyway\n");
+    printf("ubuntu_cap_init: WARNING: Lacking capability to bind ports <1024, continuing anyway\n");
   }
 
   if (Flags.secure) {
-    struct stat s;
-    char        conf_fn[200];
     if (!ubuntu_cap_have(CAP_SYS_CHROOT)) {
       printf("Cannot chroot(), but secure mode is requested.\n");
       return false;
@@ -50,24 +51,45 @@ bool ubuntu_cap_init(void) {
       printf("Cannot chroot to %s\n", Flags.chroot);
       return false;
     }
-    printf("ubuntu_cap_init: Changed root to %s\n", Flags.chroot);
     chdir("/");
+    printf("ubuntu_cap_init: chroot=%s\n", Flags.chroot);
   }
 
+  // First, set the desired gid using --group flag.
+  // If we're still running as gid=0, use the owner of the conf0.json file
   if ((Flags.gid != getgid()) && 0 != setgid(Flags.gid)) {
     printf("Cannot setgid to %d.\n", Flags.gid);
     return false;
   }
 
+  if (getgid() == 0) {
+    if (0 != setgid(s.st_gid)) {
+      printf("Cannot setgid to the group of %s (%d).\n", conf_fn, s.st_gid);
+      return false;
+    }
+    printf("ubuntu_cap_init: Set gid=%d based on %s.\n", getgid(), conf_fn);
+  }
+
+  // Now do the same for uid using --user flag.
+  // If we're still running as uid=0, use the owner of the conf0.json file
   if ((Flags.uid != getuid()) && 0 != setuid(Flags.uid)) {
     printf("Cannot setuid to %d.\n", Flags.uid);
     return false;
   }
 
+  if (getuid() == 0) {
+    if (0 != setuid(s.st_uid)) {
+      printf("Cannot setuid to the owner of %s (%d).\n", conf_fn, s.st_uid);
+      return false;
+    }
+    printf("ubuntu_cap_init: Set uid=%d based on %s.\n", getuid(), conf_fn);
+  }
+
+  // Still running as uid=0 or gid=0? Bail.
   if (getuid() == 0 || getgid() == 0) {
     printf("Refusing to run as uid=%d gid=%d.\n", getuid(), getgid());
     return false;
   }
-  printf("ubuntu_cap_init: pid=%d uid=%d gid=%d euid=%d egid=%d chroot=%s\n", getpid(), getuid(), getgid(), geteuid(), getegid(), Flags.chroot);
+  printf("ubuntu_cap_init: pid=%d uid=%d gid=%d euid=%d egid=%d\n", getpid(), getuid(), getgid(), geteuid(), getegid());
   return true;
 }
