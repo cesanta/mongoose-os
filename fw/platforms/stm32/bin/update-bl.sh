4a48741fc9d1b5c317ddb409444075f327fcab7d
Deomid Ryabkov <rojer@cesanta.com>
2019-02-12 14:43:59 +0000

    Update boot loader binaries to 1.4.1
    CL: Update boot loader binaries to 1.4.1
    
    PUBLISHED_FROM=090a42fc36efbb17907bcca9092d801bcbf0155c

--- fw/platforms/stm32/bin/update-bl.sh
+++ fw/platforms/stm32/bin/update-bl.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-for b in B-L475E-IOT01A DISCO-F746NG; do
+for b in B-L475E-IOT01A DISCO-F746NG Electron; do
   rm -f *.zip
   curl -qsSLO https://github.com/mongoose-os-apps/bootloader/releases/download/latest/bootloader-stm32-$b.zip
   unzip -p bootloader-*.zip bootloader-*/bootloader.bin > bootloader-$b.bin
