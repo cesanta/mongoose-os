b60193e1de088e08fa567cc8ddd22e6ab45372fc
Deomid Ryabkov <rojer@cesanta.com>
2019-04-24 15:49:55 +0100

    Optimize cs_crc32
    CL: none
    
    PUBLISHED_FROM=0c90866ece331a4546b9b490084ea0c23ea35682

--- common/cs_crc32.c
+++ common/cs_crc32.c
@@ -22,17 +22,20 @@
  * See "A compact CCITT crc16 and crc32 C implementation that balances processor
  * cache usage against speed".
  */
-uint32_t cs_crc32(uint32_t crc32, const void *data, uint32_t len) {
-  static const uint32_t s_crc32[16] = {
+IRAM uint32_t cs_crc32(uint32_t crc32, const void *data, uint32_t len) {
+  /* Note: volatile non-const to ensure placing in RAM instead of flash.
+   * This table is accessed a lot and flash access can be expensive. */
+  static volatile uint32_t cs_crc32_table[16] = {
       0,          0x1db71064, 0x3b6e20c8, 0x26d930ac, 0x76dc4190, 0x6b6b51f4,
       0x4db26158, 0x5005713c, 0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
-      0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c};
+      0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c,
+  };
   const uint8_t *p = (const uint8_t *) data;
   crc32 = ~crc32;
   while (len--) {
     uint8_t b = *p++;
-    crc32 = (crc32 >> 4) ^ s_crc32[(crc32 & 0xF) ^ (b & 0xF)];
-    crc32 = (crc32 >> 4) ^ s_crc32[(crc32 & 0xF) ^ (b >> 4)];
+    crc32 = (crc32 >> 4) ^ cs_crc32_table[(crc32 & 0xF) ^ (b & 0xF)];
+    crc32 = (crc32 >> 4) ^ cs_crc32_table[(crc32 & 0xF) ^ (b >> 4)];
   }
   return ~crc32;
 }
