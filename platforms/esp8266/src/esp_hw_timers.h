44e721120eb8d959b6530465d029a92f1c2fc03d
Deomid "rojer" Ryabkov <rojer@rojer.me>
2020-04-01 10:14:59 +0100

    Fix MGOS_NUM_HW_TIMERS definition

--- platforms/esp8266/src/esp_hw_timers.h
+++ platforms/esp8266/src/esp_hw_timers.h
@@ -15,14 +15,11 @@
  * limitations under the License.
  */
 
-#ifndef CS_FW_PLATFORMS_ESP8266_SRC_ESP_HW_TIMERS_H_
-#define CS_FW_PLATFORMS_ESP8266_SRC_ESP_HW_TIMERS_H_
+#pragma once
 
 #ifdef __cplusplus
 extern "C" {
-#endif /* __cplusplus */
-
-#define MGOS_NUM_HW_TIMERS 1
+#endif
 
 #define MGOS_ESP8266_HW_TIMER_NMI 0x10000
 
@@ -30,6 +27,4 @@ struct mgos_hw_timer_dev_data {};
 
 #ifdef __cplusplus
 }
-#endif /* __cplusplus */
-
-#endif /* CS_FW_PLATFORMS_ESP8266_SRC_ESP_HW_TIMERS_H_ */
+#endif
