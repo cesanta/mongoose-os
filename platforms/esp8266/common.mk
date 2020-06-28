b3c1f8a477b099a822027b27ce5fce60c1c4efef
Deomid Ryabkov <rojer@rojer.me>
2020-06-28 09:42:34 +0100

    ESP8266: Inhibit some warnings
    CL: none

--- platforms/esp8266/common.mk
+++ platforms/esp8266/common.mk
@@ -38,6 +38,8 @@ $(call compile_params,$<,$@, $(CXX), $(CXXFLAGS),"CXX")
 endef
 
 C_CXX_FLAGS  = -W -Wall -Werror -Wundef -Wno-array-bounds \
+               -Wno-error=unused-const-variable \
+               -Wno-address-of-packed-member \
                -pipe -Os -g3 \
                -D_XOPEN_SOURCE=500 \
                -nostdlib -mlongcalls -mtext-section-literals -D__ets__ \
