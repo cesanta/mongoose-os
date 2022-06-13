b93bb783dcf39066647aceaa1dc3464718b80877
Duncan Drennan <duncan@engineersimplicity.com>
2022-06-14 01:05:57 +0200

    Add ESP32-S3 support to ESP32 docker

--- tools/docker/esp32/ct_path.sh
+++ tools/docker/esp32/ct_path.sh
@@ -1,3 +1,3 @@
-export PATH="$PATH:/opt/Espressif/riscv32-esp-elf/bin:/opt/Espressif/xtensa-esp32-elf/bin"
+export PATH="$PATH:/opt/Espressif/riscv32-esp-elf/bin:/opt/Espressif/xtensa-esp32-elf/bin:/opt/Espressif/xtensa-esp32s3-elf/bin"
 export IDF_PATH=/opt/Espressif/esp-idf
 export SDK_PATH=/opt/Espressif/esp-idf
--- tools/docker/esp32c3/ct_path.sh
+++
@@ -1,2 +0,0 @@
-export PATH="$PATH:/opt/Espressif/riscv32-esp-elf/bin"
-export SDK_PATH=/opt/Espressif/esp-idf
