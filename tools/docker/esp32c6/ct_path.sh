15e5b1179f38121fb6edb541795ccc91bd8168ca
Léopold Chappuis <leopold@lchappuis.fr>
2026-07-05 14:53:31 +0200

    tools/docker: draft esp32c6-build SDK image (ESP-IDF v6.0.2)
    Based on upstream espressif/esp-idf, with
    install.sh esp32c6 instead of pinned toolchains.
    
    Signed-off-by: Léopold Chappuis <leopold@lchappuis.fr>

---
+++ tools/docker/esp32c6/ct_path.sh
@@ -0,0 +1,4 @@
+export PATH="/opt/Espressif/python_env/bin:/opt/Espressif/riscv32-esp-elf/bin:/opt/Espressif/riscv32-esp-elf-gdb/bin:/opt/Espressif/esp-idf/tools:$PATH"
+export IDF_PATH=/opt/Espressif/esp-idf
+export IDF_TOOLS_PATH=/opt/Espressif/tools
+export SDK_PATH=/opt/Espressif/esp-idf
